b1fdf01272d8bf165c11e50cf7fc4a6f0042e30d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:47:22 -0500

    feat(feature/s-1): tests/java/com/example/mocks/InMemoryTransferRepository.java

--- tests/java/com/example/mocks/InMemoryTransferRepository.java
+++ tests/java/com/example/mocks/InMemoryTransferRepository.java
@@ -1,9 +1,22 @@
 package com.example.mocks;
+
 import com.example.domain.transfer.model.TransferAggregate;
 import com.example.domain.transfer.repository.TransferRepository;
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class InMemoryTransferRepository implements TransferRepository {
     private final Map<String, TransferAggregate> store = new HashMap<>();
-    @Override public void save(TransferAggregate a) { store.put(a.id(), a); }
-    @Override public Optional<TransferAggregate> findById(String id) { return Optional.ofNullable(store.get(id)); }
+
+    @Override
+    public void save(TransferAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+    }
+
+    @Override
+    public Optional<TransferAggregate> findById(String id) {
+        return Optional.ofNullable(store.get(id));
+    }
 }

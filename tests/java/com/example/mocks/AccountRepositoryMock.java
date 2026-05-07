e629cb7b990521ceb24c9ed49859a344f011881d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:44:03 -0500

    feat(feature/s-1): tests/java/com/example/mocks/AccountRepositoryMock.java

---
+++ tests/java/com/example/mocks/AccountRepositoryMock.java
@@ -0,0 +1,22 @@
+package com.example.mocks;
+
+import com.example.domain.account.model.AccountAggregate;
+import com.example.domain.account.repository.AccountRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountRepositoryMock implements AccountRepository {
+    private final Map<String, AccountAggregate> store = new HashMap<>();
+
+    @Override
+    public AccountAggregate save(AccountAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
+    }
+
+    @Override
+    public AccountAggregate findById(String id) {
+        return store.get(id);
+    }
+}

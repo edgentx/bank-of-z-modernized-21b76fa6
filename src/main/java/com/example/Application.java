c8010b508933d1ec71afb60faf8b5238e91aca6c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:54:09 -0500

    feat(feature/s-18): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,6 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Teller System Application Entry Point
+ * <p>
+ * Tracks user-interface-navigation and teller sessions.
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

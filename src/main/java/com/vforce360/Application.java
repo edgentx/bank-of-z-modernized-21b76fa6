fb14b638879f2c7ea4dac2c4757955ad5ca2dd8d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:30:59 -0500

    feat(feature/s-1): src/main/java/com/vforce360/Application.java

--- src/main/java/com/vforce360/Application.java
+++ src/main/java/com/vforce360/Application.java
@@ -3,12 +3,8 @@ package com.vforce360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main entry point for the VForce360 Modernization Assessment Service.
- */
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

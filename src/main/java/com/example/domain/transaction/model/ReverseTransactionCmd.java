9a88a967500ba293a0ae5631eadc0531b725b860
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:07:17 -0500

    feat(feature/s-12): src/main/java/com/example/domain/transaction/model/ReverseTransactionCmd.java

--- src/main/java/com/example/domain/transaction/model/ReverseTransactionCmd.java
+++ src/main/java/com/example/domain/transaction/model/ReverseTransactionCmd.java
@@ -1,14 +1,10 @@
 package com.example.domain.transaction.model;
 
 import com.example.domain.shared.Command;
-
 import java.math.BigDecimal;
 
 public record ReverseTransactionCmd(
         String transactionId,
         String originalTransactionId,
-        BigDecimal amount,
-        String accountNumber,
-        String validationContext
-) implements Command {
-}
+        BigDecimal amount
+) implements Command {}

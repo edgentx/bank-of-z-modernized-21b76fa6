2ec86020efe50a3191fa1243c7126a59576dac3e
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:26:50 -0500

    feat(feature/s-10): tests/test/java/com/example/steps/S10RunCucumberTest.java

---
+++ tests/test/java/com/example/steps/S10RunCucumberTest.java
@@ -0,0 +1,11 @@
+package com.example.steps;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/S-10.feature")
+public class S10RunCucumberTest {
+}

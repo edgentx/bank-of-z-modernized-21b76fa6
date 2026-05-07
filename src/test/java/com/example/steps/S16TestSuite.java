a49b752e6aefb283a60b916155091ee7c15a3f72
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 09:10:53 -0500

    feat(feature/s-16): src/test/java/com/example/steps/S16TestSuite.java

--- src/test/java/com/example/steps/S16TestSuite.java
+++ src/test/java/com/example/steps/S16TestSuite.java
@@ -1,11 +1,9 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeClassnames;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@IncludeClassnames("com.example.steps.S16Steps")
-@SelectClasspathResource("features/S-16.feature")
+@SelectClasspathResource("features")
 public class S16TestSuite {
 }

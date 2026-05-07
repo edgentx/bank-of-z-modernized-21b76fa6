ff8dbad8332a06f30b99e35121ccdba0173e6323
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:07:33 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,15 +1,18 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
+/**
+ * Test Suite for Story S-FB-1.
+ * This configuration allows JUnit 5 to run the Cucumber features defined in resources.
+ */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@SelectClasspathResource("features/S-FB-1.feature")
+@Cucumber
 public class SFB1TestSuite {
+    // Test execution is handled by the Cucumber engine
 }

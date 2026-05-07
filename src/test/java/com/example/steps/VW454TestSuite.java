f1f779b99906f261e5c4df8321a172980b9be2c9
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 14:45:24 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,16 +1,34 @@
 package com.example.steps;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import com.example.mocks.MockSlackNotificationPort;
+import com.example.ports.SlackNotificationPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "src/test/resources/features/VW454.feature",
-    glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber/VW454.html"},
-    monochrome = true
-)
+/**
+ * Test Suite for VW-454 Regression.
+ * Configures the Spring Test Context to use Mock Adapters.
+ */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasses(VW454Steps.class)
+@SpringBootTest(classes = VW454TestSuite.TestConfig.class)
+@Cucumber
 public class VW454TestSuite {
-    // Test Suite Entry Point
+
+    @Configuration
+    static class TestConfig {
+        
+        @Bean
+        @Primary
+        public SlackNotificationPort slackNotificationPort() {
+            return new MockSlackNotificationPort();
+        }
+    }
 }

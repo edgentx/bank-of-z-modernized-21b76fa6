916886d621078b4a276a56ae4a7fff66bf2d745f
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 11:12:05 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/repository/TellerSessionRepository.java

--- src/main/java/com/example/domain/tellersession/repository/TellerSessionRepository.java
+++ src/main/java/com/example/domain/tellersession/repository/TellerSessionRepository.java
@@ -5,6 +5,7 @@ import com.example.domain.tellersession.model.TellerSessionAggregate;
 import java.util.Optional;
 
 public interface TellerSessionRepository {
+    TellerSessionAggregate save(TellerSessionAggregate aggregate);
     Optional<TellerSessionAggregate> findById(String id);
-    void save(TellerSessionAggregate aggregate);
+    void deleteById(String id);
 }

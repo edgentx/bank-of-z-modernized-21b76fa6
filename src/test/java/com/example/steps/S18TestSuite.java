03b349d3e0cf5a39c2f305988d9dee3f08715b80
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 10:40:51 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,18 +1,11 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-/**
- * JUnit 5 Suite configuration for running Cucumber features in the 'features' directory.
- * S-18: Teller Session Feature Test Suite.
- */
 @Suite
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/S-18.feature")
 public class S18TestSuite {
-    // This class acts as the entry point for JUnit to run the Cucumber tests
 }

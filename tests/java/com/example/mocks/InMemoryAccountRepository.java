1e3ded91f3027f4f26313a2a14928dd8ed5a1431
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:45:04 -0500

    feat(feature/s-1): tests/java/com/example/mocks/InMemoryAccountRepository.java

--- tests/java/com/example/mocks/InMemoryAccountRepository.java
+++ tests/java/com/example/mocks/InMemoryAccountRepository.java
@@ -2,20 +2,21 @@ package com.example.mocks;
 
 import com.example.domain.account.model.AccountAggregate;
 import com.example.domain.account.repository.AccountRepository;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class InMemoryAccountRepository implements AccountRepository {
     private final Map<String, AccountAggregate> store = new HashMap<>();
 
     @Override
-    public void save(AccountAggregate aggregate) {
+    public AccountAggregate save(AccountAggregate aggregate) {
         store.put(aggregate.id(), aggregate);
+        return aggregate;
     }
 
     @Override
-    public Optional<AccountAggregate> findById(String id) {
-        return Optional.ofNullable(store.get(id));
+    public AccountAggregate findById(String id) {
+        return store.get(id);
     }
 }

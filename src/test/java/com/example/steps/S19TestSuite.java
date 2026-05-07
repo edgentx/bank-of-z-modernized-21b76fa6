84efae11b0473bf59f4ce95d80f8a6c6ed40b9ba
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 18:15:33 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -4,6 +4,10 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite for S-19 Feature: NavigateMenuCmd.
+ * Uses JUnit 5 Platform Suite to run Cucumber via the JUnit Platform Engine.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-19.feature")

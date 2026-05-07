1db6fdb44035f873f3f58d578e34784a51a4fa45
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 12:04:15 -0500

    feat(feature/s-18): src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java

--- src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
+++ src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
@@ -7,7 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * In-memory implementation of the TellerSessionRepository for testing.
+ */
 public class InMemoryTellerSessionRepository implements TellerSessionRepository {
+
     private final Map<String, TellerSessionAggregate> store = new HashMap<>();
 
     @Override
@@ -18,5 +22,12 @@ public class InMemoryTellerSessionRepository implements TellerSessionRepository 
     @Override
     public Optional<TellerSessionAggregate> findById(String id) {
         return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public TellerSessionAggregate create(String id) {
+        TellerSessionAggregate aggregate = new TellerSessionAggregate(id);
+        store.put(id, aggregate);
+        return aggregate;
     }
 }

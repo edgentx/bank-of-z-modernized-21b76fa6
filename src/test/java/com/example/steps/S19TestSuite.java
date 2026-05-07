23e037c1f8175db28dc7d166b5cd0ab7538e5a40
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 08:42:44 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -1,15 +1,11 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-19.feature")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@SelectClasspathResource("features")
 public class S19TestSuite {
 }

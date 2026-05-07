d574f4894ab59491be839dcc53031f3e3599e45a
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:29:50 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubIssuePort.java

--- src/test/java/com/example/mocks/MockGitHubIssuePort.java
+++ src/test/java/com/example/mocks/MockGitHubIssuePort.java
@@ -1,18 +1,32 @@
 package com.example.mocks;
 
 import com.example.ports.GitHubIssuePort;
+import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Mock implementation of GitHubIssuePort for testing.
+ */
+@Component
 public class MockGitHubIssuePort implements GitHubIssuePort {
-    private String baseUrl = "http://github.com/example/issues/";
-    private int counter = 1;
+
+    private String responseUrl = "https://github.com/mock-org/mock-repo/issues/454";
+    private boolean shouldFail = false;
 
     @Override
-    public String createIssue(String title, String body) {
-        // Simulate GitHub returning a URL to the new issue
-        return baseUrl + counter++;
+    public CompletableFuture<String> createIssue(String title, String body) {
+        if (shouldFail) {
+            return CompletableFuture.failedFuture(new RuntimeException("Simulated GitHub failure"));
+        }
+        return CompletableFuture.completedFuture(responseUrl);
     }
 
-    public void setBaseUrl(String url) {
-        this.baseUrl = url;
+    public void setResponseUrl(String url) {
+        this.responseUrl = url;
+    }
+
+    public void setSimulatedFailure(boolean fail) {
+        this.shouldFail = fail;
     }
 }

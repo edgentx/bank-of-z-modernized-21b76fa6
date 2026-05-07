fb137947676add597b6c867cdb93cc6516d30306
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 14:44:25 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/SlackNotificationPort.java

--- src/main/java/com/example/ports/SlackNotificationPort.java
+++ src/main/java/com/example/ports/SlackNotificationPort.java
@@ -4,5 +4,9 @@ package com.example.ports;
  * Port interface for sending Slack notifications.
  */
 public interface SlackNotificationPort {
+    /**
+     * Sends a notification to the configured Slack channel.
+     * @param messageBody The formatted message to send.
+     */
     void notify(String messageBody);
 }

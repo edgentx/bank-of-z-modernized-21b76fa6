8c35540240a69e47b40c99098f9d1e12b4770589
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:43:32 -0500

    feat(feature/s-1): src/main/java/com/vforce360/ports/MarReportPort.java

--- src/main/java/com/vforce360/ports/MarReportPort.java
+++ src/main/java/com/vforce360/ports/MarReportPort.java
@@ -1,10 +1,1 @@
-package com.vforce360.ports;
-
-import com.vforce360.model.MarReport;
-
-/**
- * Port interface for retrieving MAR data.
- */
-public interface MarReportPort {
-    MarReport findByProjectId(String projectId);
-}
+...

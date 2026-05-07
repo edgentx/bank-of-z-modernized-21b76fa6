d9d9310851c5d5b94c8bf04996abeaaa8141f836
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 08:56:58 -0500

    feat(feature/s-16): src/test/java/com/example/steps/S16TestSuite.java

--- src/test/java/com/example/steps/S16TestSuite.java
+++ src/test/java/com/example/steps/S16TestSuite.java
@@ -1,11 +1,16 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.CucumberJUnitPlatformExecutionMode.SCENARIO;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-16.feature")
+@ConfigurationParameter(key = "cucumber.execution-mode", value = SCENARIO)
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "not @Skip")
 public class S16TestSuite {
 }

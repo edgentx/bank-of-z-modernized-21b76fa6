bc94c15dad09443272865976f5140653b007efaa
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 11:33:15 -0500

    feat(feature/s-21): src/test/java/com/example/steps/S21TestSuite.java

--- src/test/java/com/example/steps/S21TestSuite.java
+++ src/test/java/com/example/steps/S21TestSuite.java
@@ -3,12 +3,13 @@ package com.example.steps;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.ConfigurationParameter;
 
-/**
- * Test Suite for Cucumber S-21 Features.
- */
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-21.feature")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S21TestSuite {
 }

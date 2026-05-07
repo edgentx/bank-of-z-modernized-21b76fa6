886a64f7881ad558816116a8f6a4f43bcd9b7d5c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 14:48:35 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -1,9 +1,15 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite for Story S-19: TellerSession.
+ * Connects JUnit 5 Platform to Cucumber features.
+ */
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-19.feature")
 public class S19TestSuite {
 }

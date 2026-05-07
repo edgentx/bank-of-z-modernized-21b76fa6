baf1f57e92a8a36d0d2dbcb2d1e0199e68bef9f7
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 16:00:35 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,17 +1,13 @@
 package com.example.steps;
 
-import io.cucumber.junit.platform.engine.Cucumber;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-/**
- * Test Suite for S-FB-1.
- * Located in src/test/java to be picked up by JUnit Platform.
- */
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@Cucumber
-public class SFB1TestSuite {
-}
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = {"features/S-FB-1.feature"},
+    glue = {"com.example.steps"},
+    plugin = {"pretty", "html:target/cucumber-report"}
+)
+public class SFB1TestSuite {}

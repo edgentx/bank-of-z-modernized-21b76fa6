901d5535751e4a917c857b8432d83482a9bdbd32
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 11:29:31 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java

--- src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java
+++ src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java
@@ -4,20 +4,9 @@ import com.example.domain.shared.DomainEvent;
 
 import java.time.Instant;
 
-public record SessionEndedEvent(
-    String aggregateId,
-    Instant occurredAt
-) implements DomainEvent {
+public record SessionEndedEvent(String aggregateId, Instant occurredAt) implements DomainEvent {
     @Override
     public String type() {
         return "session.ended";
-    }
-    @Override
-    public String aggregateId() {
-        return aggregateId;
-    }
-    @Override
-    public Instant occurredAt() {
-        return occurredAt;
     }
 }

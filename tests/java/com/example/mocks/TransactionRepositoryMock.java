c5173d94b69e807f8a0d3db37c25f97cb24395ee
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:44:05 -0500

    feat(feature/s-1): tests/java/com/example/mocks/TransactionRepositoryMock.java

---
+++ tests/java/com/example/mocks/TransactionRepositoryMock.java
@@ -0,0 +1,22 @@
+package com.example.mocks;
+
+import com.example.domain.transaction.model.TransactionAggregate;
+import com.example.domain.transaction.repository.TransactionRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TransactionRepositoryMock implements TransactionRepository {
+    private final Map<String, TransactionAggregate> store = new HashMap<>();
+
+    @Override
+    public TransactionAggregate save(TransactionAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
+    }
+
+    @Override
+    public TransactionAggregate findById(String id) {
+        return store.get(id);
+    }
+}

ada431b65ab58bba47aafd5ee4ce1e8e605f5489
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:31:38 -0500

    feat(feature/s-1): src/main/java/com/vforce360/Application.java

--- src/main/java/com/vforce360/Application.java
+++ src/main/java/com/vforce360/Application.java
@@ -3,6 +3,10 @@ package com.vforce360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application entry point for the Modernization Assessment Report (MAR) service.
+ * This application serves report data to the Next.js frontend.
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

d64d174458ec942eb5b28b2b8c17b8d0ad426353
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:51:59 -0500

    feat(feature/s-11): src/test/java/com/example/steps/CucumberTestSuite.java

---
+++ src/test/java/com/example/steps/CucumberTestSuite.java
@@ -0,0 +1,12 @@
+package com.example.steps;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class CucumberTestSuite {
+    // This class acts as the test runner for Cucumber
+}

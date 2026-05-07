361f20831341ec3ff443b60f07d7065a9f20fb7d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:17:46 -0500

    feat(feature/s-19): src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java

--- src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
+++ src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
@@ -1,15 +1,14 @@
 package com.example.mocks;
 
-import com.example.domain.tellersession.model.TellerSessionAggregate;
-import com.example.domain.tellersession.repository.TellerSessionRepository;
+import com.example.domain.teller.model.TellerSessionAggregate;
+import com.example.domain.teller.repository.TellerSessionRepository;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryTellerSessionRepository implements TellerSessionRepository {
-    private final Map<String, TellerSessionAggregate> store = new ConcurrentHashMap<>();
+    private final Map<String, TellerSessionAggregate> store = new HashMap<>();
 
     @Override
     public TellerSessionAggregate save(TellerSessionAggregate aggregate) {

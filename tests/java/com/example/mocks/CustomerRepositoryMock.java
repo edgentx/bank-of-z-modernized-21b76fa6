a34d298e134a4598fc67bd99f9d38f2fc27dcfb7
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:44:02 -0500

    feat(feature/s-1): tests/java/com/example/mocks/CustomerRepositoryMock.java

---
+++ tests/java/com/example/mocks/CustomerRepositoryMock.java
@@ -0,0 +1,22 @@
+package com.example.mocks;
+
+import com.example.domain.customer.model.CustomerAggregate;
+import com.example.domain.customer.repository.CustomerRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRepositoryMock implements CustomerRepository {
+    private final Map<String, CustomerAggregate> store = new HashMap<>();
+
+    @Override
+    public CustomerAggregate save(CustomerAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
+    }
+
+    @Override
+    public CustomerAggregate findById(String id) {
+        return store.get(id);
+    }
+}

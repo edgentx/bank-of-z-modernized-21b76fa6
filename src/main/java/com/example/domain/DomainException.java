d26aa6ad5376015b34c553a40e8074ba38f82595
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:50:43 -0500

    feat(feature/s-12): src/main/java/com/example/domain/DomainException.java

--- src/main/java/com/example/domain/DomainException.java
+++ src/main/java/com/example/domain/DomainException.java
@@ -1,6 +1,10 @@
 package com.example.domain;
 
+/**
+ * Base exception for Domain logic violations.
+ */
 public class DomainException extends RuntimeException {
+
     public DomainException(String message) {
         super(message);
     }

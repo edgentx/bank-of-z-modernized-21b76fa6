b04016d0f73ddb9936f73c696af3cbd4540a9a02
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 16:09:04 -0500

    feat(feature/s-fb-1): src/main/java/com/example/infrastructure/config/GitHubProperties.java

--- src/main/java/com/example/infrastructure/config/GitHubProperties.java
+++ src/main/java/com/example/infrastructure/config/GitHubProperties.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "github")
 public class GitHubProperties {
     private String baseUrl = "https://github.com/bank-of-z/issues/";
+    private String token;
+    private String repoOwner;
+    private String repoName;
 
     public String getBaseUrl() {
         return baseUrl;
@@ -14,5 +17,29 @@ public class GitHubProperties {
 
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRepoOwner() {
+        return repoOwner;
+    }
+
+    public void setRepoOwner(String repoOwner) {
+        this.repoOwner = repoOwner;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
     }
 }

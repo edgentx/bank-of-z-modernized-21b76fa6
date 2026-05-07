29f94a312d275c9ffa3d006667f3c7866b46972b
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:50:45 -0500

    feat(feature/s-12): src/main/java/com/example/domain/S12Event.java

--- src/main/java/com/example/domain/S12Event.java
+++ src/main/java/com/example/domain/S12Event.java
@@ -1,27 +1,7 @@
 package com.example.domain;
 
-import java.util.UUID;
-
-public class S12Event {
-    private final String type;
-    private final UUID transactionId;
-    private final UUID originalTransactionId;
-
-    public S12Event(UUID transactionId, UUID originalTransactionId) {
-        this.type = "TransactionReversed";
-        this.transactionId = transactionId;
-        this.originalTransactionId = originalTransactionId;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public UUID getTransactionId() {
-        return transactionId;
-    }
-
-    public UUID getOriginalTransactionId() {
-        return originalTransactionId;
-    }
+/**
+ * Marker interface for S-12 Events.
+ */
+public interface S12Event {
 }

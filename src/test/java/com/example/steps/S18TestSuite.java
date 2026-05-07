4df126d5d1c9bd0e165b2d58909839edbbc932c9
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 08:45:01 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,11 +1,9 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 public class S18TestSuite {
 }

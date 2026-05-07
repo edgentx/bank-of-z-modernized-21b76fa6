1c48dbcf0a21dc7ecd887c40eef4794bac9b6191
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:10:45 -0500

    feat(feature/s-10): src/main/java/com/example/domain/PostDepositCmd.java

--- src/main/java/com/example/domain/PostDepositCmd.java
+++ src/main/java/com/example/domain/PostDepositCmd.java
@@ -1,33 +1,7 @@
 package com.example.domain;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
-public class PostDepositCmd {
-    private String accountNumber;
-    private BigDecimal amount;
-    private String currency;
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
+public record PostDepositCmd(String accountNumber, BigDecimal amount, Currency currency) {
 }

9cf862a7c0cc36bae76ac494bdfa48abea0e455e
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 08:11:04 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,11 +1,13 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.CucumberJUnitPlatformRunnerOptions.GLUE_ENABLED;
+
 @Suite
-@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_ENABLED, value = "true")
 public class S18TestSuite {
 }

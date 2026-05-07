70162e5ab1fc15c3e12d9fdb6d3e6cb66b424fc4
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:26:55 -0500

    feat(feature/s-10): src/main/java/com/example/domain/transaction/model/PostWithdrawalCmd.java

--- src/main/java/com/example/domain/transaction/model/PostWithdrawalCmd.java
+++ src/main/java/com/example/domain/transaction/model/PostWithdrawalCmd.java
@@ -1,4 +1,7 @@
 package com.example.domain.transaction.model;
+
 import com.example.domain.shared.Command;
 import java.math.BigDecimal;
-public record PostWithdrawalCmd(String transactionId, String accountId, BigDecimal amount, String currency) implements Command {}
+
+public record PostWithdrawalCmd(String transactionId, String accountId, BigDecimal amount, String currency) implements Command {
+}

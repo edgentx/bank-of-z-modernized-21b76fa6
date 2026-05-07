677e0fb2f43ed5e1a7d9d65966de93a3c74a2485
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 16:08:49 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/defect/model/ReportDefectCmd.java

--- src/main/java/com/example/domain/defect/model/ReportDefectCmd.java
+++ src/main/java/com/example/domain/defect/model/ReportDefectCmd.java
@@ -2,8 +2,4 @@ package com.example.domain.defect.model;
 
 import com.example.domain.shared.Command;
 
-public record ReportDefectCmd(
-        String defectId,
-        String title,
-        String description
-) implements Command {}
+public record ReportDefectCmd(String defectId, String title, String description) implements Command {}

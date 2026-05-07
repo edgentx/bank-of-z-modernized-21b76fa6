3e8f6dbac0c79d869a31b0190c539605e04e306b
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:14:07 -0500

    feat(feature/s-1): src/main/java/com/vforce360/Application.java

--- src/main/java/com/vforce360/Application.java
+++ src/main/java/com/vforce360/Application.java
@@ -3,8 +3,12 @@ package com.vforce360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Boot Application entry point.
+ */
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

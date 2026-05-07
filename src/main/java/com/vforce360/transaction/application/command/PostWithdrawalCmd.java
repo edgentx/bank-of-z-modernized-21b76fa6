dce5bbd6636f28e3d7a68779a5ec0152c8a0078a
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:42:25 -0500

    feat(feature/s-11): src/main/java/com/vforce360/transaction/application/command/PostWithdrawalCmd.java

---
+++ src/main/java/com/vforce360/transaction/application/command/PostWithdrawalCmd.java
@@ -0,0 +1,36 @@
+package com.vforce360.transaction.application.command;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Objects;
+
+public class PostWithdrawalCmd {
+    private final String accountNumber;
+    private final BigDecimal amount;
+    private final Currency currency;
+
+    public PostWithdrawalCmd(String accountNumber, BigDecimal amount, Currency currency) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public String getAccountNumber() { return accountNumber; }
+    public BigDecimal getAmount() { return amount; }
+    public Currency getCurrency() { return currency; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostWithdrawalCmd that = (PostWithdrawalCmd) o;
+        return Objects.equals(accountNumber, that.accountNumber) &&
+                Objects.equals(amount, that.amount) &&
+                Objects.equals(currency, that.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, amount, currency);
+    }
+}

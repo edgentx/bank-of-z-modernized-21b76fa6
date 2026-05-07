9a9959ccaaa7a0325093d0a20633d402ac9459e8
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:39:45 -0500

    feat(feature/s-13): src/test/java/com/example/steps/S13TestSuite.java

--- src/test/java/com/example/steps/S13TestSuite.java
+++ src/test/java/com/example/steps/S13TestSuite.java
@@ -4,9 +4,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-13 Cucumber Tests.
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-13.feature")

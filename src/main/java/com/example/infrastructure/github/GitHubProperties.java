62ae3a16c049c7404defa0ecee56d8125364454c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:30:43 -0500

    feat(feature/s-fb-1): src/main/java/com/example/infrastructure/github/GitHubProperties.java

---
+++ src/main/java/com/example/infrastructure/github/GitHubProperties.java
@@ -0,0 +1,18 @@
+package com.example.infrastructure.github;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "github")
+public class GitHubProperties {
+    private String repoUrl;
+
+    public String getRepoUrl() {
+        return repoUrl;
+    }
+
+    public void setRepoUrl(String repoUrl) {
+        this.repoUrl = repoUrl;
+    }
+}

00abf30e6094d4e5a2dffcf2d3a8c6e386387951
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:43:56 -0500

    feat(feature/s-1): src/main/java/com/example/domain/transaction/repository/TransactionRepository.java

--- src/main/java/com/example/domain/transaction/repository/TransactionRepository.java
+++ src/main/java/com/example/domain/transaction/repository/TransactionRepository.java
@@ -1,8 +1,8 @@
 package com.example.domain.transaction.repository;
 
 import com.example.domain.transaction.model.TransactionAggregate;
-import java.util.Optional;
+
 public interface TransactionRepository {
-    void save(TransactionAggregate aggregate);
-    Optional<TransactionAggregate> findById(String id);
+    TransactionAggregate save(TransactionAggregate aggregate);
+    TransactionAggregate findById(String id);
 }

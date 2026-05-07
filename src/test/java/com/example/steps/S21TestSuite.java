800a82aed25fbdf63f646ad581a539849c80b06d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 11:15:45 -0500

    feat(feature/s-21): src/test/java/com/example/steps/S21TestSuite.java

--- src/test/java/com/example/steps/S21TestSuite.java
+++ src/test/java/com/example/steps/S21TestSuite.java
@@ -6,6 +6,6 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-21.feature")
 public class S21TestSuite {
 }

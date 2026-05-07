c96ea328fd50354c84bb7085348fe9dad8925070
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:53:21 -0500

    feat(feature/s-11): src/main/java/com/example/domain/PostWithdrawalCmd.java

--- src/main/java/com/example/domain/PostWithdrawalCmd.java
+++ src/main/java/com/example/domain/PostWithdrawalCmd.java
@@ -2,6 +2,12 @@ package com.example.domain;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 public record PostWithdrawalCmd(String accountNumber, BigDecimal amount, Currency currency) {
+    public PostWithdrawalCmd {
+        Objects.requireNonNull(accountNumber, "accountNumber cannot be null");
+        Objects.requireNonNull(amount, "amount cannot be null");
+        Objects.requireNonNull(currency, "currency cannot be null");
+    }
 }

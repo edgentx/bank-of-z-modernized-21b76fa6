9bdb9dd016b40cff54543b77c08e7bfc9990b14c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 14:36:23 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Main Application Entry Point.
+ * Scans components, services, and ports.
+ */
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
+@ComponentScan(basePackages = {"com.example"})
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

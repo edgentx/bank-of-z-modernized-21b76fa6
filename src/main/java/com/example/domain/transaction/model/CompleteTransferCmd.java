3fc5822b633dbace70c5562b9c5070b4a6bfc0b1
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:43:16 -0500

    feat(feature/s-14): src/main/java/com/example/domain/transaction/model/CompleteTransferCmd.java

--- src/main/java/com/example/domain/transaction/model/CompleteTransferCmd.java
+++ src/main/java/com/example/domain/transaction/model/CompleteTransferCmd.java
@@ -1,13 +1,5 @@
 package com.example.domain.transaction.model;
 
 import com.example.domain.shared.Command;
-import java.math.BigDecimal;
 
-public record CompleteTransferCmd(
-    String transferId,
-    String fromAccountId,
-    String toAccountId,
-    BigDecimal amount,
-    BigDecimal availableBalance,
-    boolean atomicStateValid
-) implements Command {}
+public record CompleteTransferCmd(String transferReference) implements Command {}

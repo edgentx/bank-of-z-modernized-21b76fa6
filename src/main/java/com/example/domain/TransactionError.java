50fd532449bf905349667e1861e88a23b46a62f5
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:15:53 -0500

    feat(feature/s-10): src/main/java/com/example/domain/TransactionError.java

--- src/main/java/com/example/domain/TransactionError.java
+++ src/main/java/com/example/domain/TransactionError.java
@@ -1,6 +1,6 @@
 package com.example.domain;
 
-public class TransactionError extends RuntimeException {
+public class TransactionError extends Exception {
     public TransactionError(String message) {
         super(message);
     }

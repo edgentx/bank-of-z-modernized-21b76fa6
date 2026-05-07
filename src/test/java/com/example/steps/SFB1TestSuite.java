9bcf3af4484d1a498e9d7aa3f622016f5cfef310
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 12:47:54 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

---
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -0,0 +1,15 @@
+package com.example.steps;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "features/S-FB-1.feature",
+    glue = {"com.example.steps"},
+    plugin = {"pretty", "html:target/cucumber-report/S-FB-1.html"}
+)
+public class SFB1TestSuite {
+    // Suite entry point
+}

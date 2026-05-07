da75b35a3c9eecd06eaa423fec6a6dba409d5e65
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:15:27 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -8,5 +8,4 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-20.feature")
 public class S20TestSuite {
-    // JUnit 5 Suite to run Cucumber features via JUnit Platform
 }

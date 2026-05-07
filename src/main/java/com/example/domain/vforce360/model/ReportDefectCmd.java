6e664b7f2889e73907cc8cd30ce4bfa26f5f68b9
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 17:34:06 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java

--- src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java
+++ src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java
@@ -2,13 +2,4 @@ package com.example.domain.vforce360.model;
 
 import com.example.domain.shared.Command;
 
-import java.util.Map;
-
-public record ReportDefectCmd(
-        String defectId,
-        String title,
-        Map<String, Object> details,
-        String expectedBehavior,
-        String actualBehavior,
-        String slackChannelId
-) implements Command {}
+public record ReportDefectCmd(String aggregateId, String githubUrl) implements Command {}

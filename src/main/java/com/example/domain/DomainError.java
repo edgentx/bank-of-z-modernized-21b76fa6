0df3ae98d7c8b34dadf3ed63b4670491233c6514
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:29:23 -0500

    feat(feature/s-10): src/main/java/com/example/domain/DomainError.java

--- src/main/java/com/example/domain/DomainError.java
+++ src/main/java/com/example/domain/DomainError.java
@@ -1,11 +1,11 @@
 package com.example.domain;
 
-/**
- * Domain Exception representing a violation of business rules.
- */
 public class DomainError extends RuntimeException {
-
     public DomainError(String message) {
         super(message);
+    }
+
+    public DomainError(String message, Throwable cause) {
+        super(message, cause);
     }
 }

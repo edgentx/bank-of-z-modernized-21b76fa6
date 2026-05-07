6b3031cfd37848835925e2e73e5b748d8575a32a
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:00:36 -0500

    feat(feature/s-12): src/main/java/com/example/domain/transaction/model/ReverseTransactionCmd.java

---
+++ src/main/java/com/example/domain/transaction/model/ReverseTransactionCmd.java
@@ -0,0 +1,5 @@
+package com.example.domain.transaction.model;
+
+import com.example.domain.shared.Command;
+
+public record ReverseTransactionCmd(String transactionId, String originalTransactionId) implements Command {}

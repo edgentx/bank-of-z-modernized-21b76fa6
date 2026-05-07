53f6d48b009b187a74e2a9e226134c28011e8f96
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 17:01:46 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,15 +1,11 @@
 package com.example.steps;
 
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite configuration for Cucumber tests related to Story S-FB-1.
- * This allows running the feature file via standard JUnit 5 engines.
- */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-FB-1.feature")
+@SelectClasses({SFB1DefectReportingSteps.class})
 public class SFB1TestSuite {
 }

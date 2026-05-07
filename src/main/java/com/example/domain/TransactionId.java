eadca1f873186fba0c62d77faf45a52cd17e1490
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:48:57 -0500

    feat(feature/s-12): src/main/java/com/example/domain/TransactionId.java

---
+++ src/main/java/com/example/domain/TransactionId.java
@@ -0,0 +1,29 @@
+package com.example.domain;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class TransactionId {
+    private final UUID value;
+
+    public TransactionId(UUID value) {
+        this.value = value;
+    }
+
+    public UUID value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionId that = (TransactionId) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}

4df4f644a3b5ee01f99172359f39d47ebf0d95de
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:17:10 -0500

    feat(feature/s-19): src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java

--- src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java
+++ src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java
@@ -1,12 +1,11 @@
 package com.example.domain.tellersession.repository;
 
 import com.example.domain.tellersession.model.TellerSessionAggregate;
-import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
 public class InMemoryTellerSessionRepository implements TellerSessionRepository {
     private final Map<String, TellerSessionAggregate> store = new HashMap<>();
 

ddaf0056ba086745c3216b7605c6bd5755b24e72
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 10:28:27 -0500

    feat(feature/s-19): src/main/java/com/example/domain/tellsession/model/NavigateMenuCmd.java

---
+++ src/main/java/com/example/domain/tellsession/model/NavigateMenuCmd.java
@@ -0,0 +1,6 @@
+package com.example.domain.tellsession.model;
+
+import com.example.domain.shared.Command;
+
+public record NavigateMenuCmd(String sessionId, String menuId, String action) implements Command {
+}

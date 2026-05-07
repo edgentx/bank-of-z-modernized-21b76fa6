3dce94b27d37ecf1e88a732ce2aa2b76cf6b6cee
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 07:10:25 -0500

    feat(feature/s-13): src/test/java/com/example/steps/S13TestRunner.java

---
+++ src/test/java/com/example/steps/S13TestRunner.java
@@ -0,0 +1,11 @@
+package com.example.steps;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/S-13.feature")
+public class S13TestRunner {
+}

311bc5022608b1fd6094a283f2763b57c5406a6b
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:42:06 -0500

    feat(feature/s-1): src/main/java/com/example/domain/transfer/repository/TransferRepository.java

--- src/main/java/com/example/domain/transfer/repository/TransferRepository.java
+++ src/main/java/com/example/domain/transfer/repository/TransferRepository.java
@@ -1,8 +1,8 @@
 package com.example.domain.transfer.repository;
 
 import com.example.domain.transfer.model.TransferAggregate;
-
+import java.util.Optional;
 public interface TransferRepository {
-    TransferAggregate save(TransferAggregate aggregate);
-    TransferAggregate findById(String id);
+    void save(TransferAggregate aggregate);
+    Optional<TransferAggregate> findById(String id);
 }

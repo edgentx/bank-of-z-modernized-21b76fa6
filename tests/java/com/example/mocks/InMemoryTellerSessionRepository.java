31e7b72fba8a562456d41fdf742957435e3b0c2c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:46:12 -0500

    feat(feature/s-1): tests/java/com/example/mocks/InMemoryTellerSessionRepository.java

--- tests/java/com/example/mocks/InMemoryTellerSessionRepository.java
+++ tests/java/com/example/mocks/InMemoryTellerSessionRepository.java
@@ -1,22 +1,9 @@
 package com.example.mocks;
-
 import com.example.domain.teller.model.TellerSessionAggregate;
 import com.example.domain.teller.repository.TellerSessionRepository;
-
-import java.util.HashMap;
-    java.util.Map;
-
+import java.util.*;
 public class InMemoryTellerSessionRepository implements TellerSessionRepository {
     private final Map<String, TellerSessionAggregate> store = new HashMap<>();
-
-    @Override
-    public TellerSessionAggregate save(TellerSessionAggregate aggregate) {
-        store.put(aggregate.id(), aggregate);
-        return aggregate;
-    }
-
-    @Override
-    public TellerSessionAggregate findById(String id) {
-        return store.get(id);
-    }
+    @Override public void save(TellerSessionAggregate a) { store.put(a.id(), a); }
+    @Override public Optional<TellerSessionAggregate> findById(String id) { return Optional.ofNullable(store.get(id)); }
 }

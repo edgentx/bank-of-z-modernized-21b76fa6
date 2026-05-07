5c2eadf5e9ae67cff1d050e4fad590fc9aad5ae6
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:46:09 -0500

    feat(feature/s-1): tests/java/com/example/mocks/MockTransferRepository.java

--- tests/java/com/example/mocks/MockTransferRepository.java
+++ tests/java/com/example/mocks/MockTransferRepository.java
@@ -2,12 +2,10 @@ package com.example.mocks;
 
 import com.example.domain.transfer.model.TransferAggregate;
 import com.example.domain.transfer.repository.TransferRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Repository
 public class MockTransferRepository implements TransferRepository {
     private final Map<String, TransferAggregate> store = new HashMap<>();
 

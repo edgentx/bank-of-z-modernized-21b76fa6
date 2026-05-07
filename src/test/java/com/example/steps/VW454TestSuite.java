cb2c9c87fc76d50b0401e0880e89e42c365c92ac
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 17:39:34 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,15 +1,21 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
-import org.junit.platform.suite.api.SelectClasses;
+import io.cucumber.core.options.Constants;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 /**
- * Test Suite to run the Cucumber test for S-FB-1.
+ * Test Suite for VW-454 Regression.
+ * This runs the specific feature file for this defect.
  */
 @Suite
-@IncludeClassNamePatterns(".*Test")
-@SelectClasses(VW454Steps.class)
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/VW454.feature")
+@CucumberContextConfiguration
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.example.steps")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
 public class VW454TestSuite {
-    // JUnit 5 Suite configuration
 }

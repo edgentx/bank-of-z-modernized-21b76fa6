ad3248f0035f98a89898f8082f3c9f3f7a94842d
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:36:23 -0500

    feat(feature/s-10): src/test/java/com/example/runners/CucumberTestSuite.java

--- src/test/java/com/example/runners/CucumberTestSuite.java
+++ src/test/java/com/example/runners/CucumberTestSuite.java
@@ -1,11 +1,17 @@
 package com.example.runners;
 
-import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+/**
+ * JUnit 5 Suite wrapper for running Cucumber features.
+ * This allows standard Maven test execution to pick up BDD scenarios.
+ */
 @Suite
-@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class CucumberTestSuite {
 }

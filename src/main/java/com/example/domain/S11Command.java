36ed5d31e60e52a89daef246ae21c46aa7e0d851
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:45:03 -0500

    feat(feature/s-11): src/main/java/com/example/domain/S11Command.java

---
+++ src/main/java/com/example/domain/S11Command.java
@@ -0,0 +1,34 @@
+package com.example.domain;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class S11Command {
+    private String accountNumber;
+    private BigDecimal amount;
+    private Currency currency;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+}

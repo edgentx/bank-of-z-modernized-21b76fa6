48f0ffbda4a726023592fcc246e63afef2c829c2
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 14:19:26 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,15 +1,14 @@
 package com.example.steps;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-FB-1.
- * Connects Cucumber features to the JUnit Platform.
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-FB-1.feature")
+@Cucumber
 public class SFB1TestSuite {
+    // Cucumber test suite configuration
 }

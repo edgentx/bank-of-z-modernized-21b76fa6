a7fd15432d77fd61bc031ac98112b9746cabd8f7
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 16:09:01 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,29 @@
 package com.example;
 
+import com.example.adapters.GitHubAdapter;
+import com.example.adapters.SlackAdapter;
+import com.example.ports.GitHubPort;
+import com.example.ports.SlackPort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.domain", "com.example.ports", "com.example.adapters"})
+@EnableFeignClients
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public GitHubPort gitHubPort(GitHubAdapter adapter) {
+        return adapter;
+    }
+
+    @Bean
+    public SlackPort slackPort(SlackAdapter adapter) {
+        return adapter;
     }
 }

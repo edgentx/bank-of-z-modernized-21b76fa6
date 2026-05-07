ccc51a4416363d9360f8973ad9b2e89477a5de4c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:50:44 -0500

    feat(feature/s-12): src/main/java/com/example/domain/S12Command.java

--- src/main/java/com/example/domain/S12Command.java
+++ src/main/java/com/example/domain/S12Command.java
@@ -1,29 +1,7 @@
 package com.example.domain;
 
-import java.util.Objects;
-import java.util.UUID;
-
-public class S12Command {
-    private final UUID originalTransactionId;
-
-    public S12Command(TransactionId originalTransactionId) {
-        this.originalTransactionId = originalTransactionId.value();
-    }
-
-    public UUID getOriginalTransactionId() {
-        return originalTransactionId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        S12Command that = (S12Command) o;
-        return Objects.equals(originalTransactionId, that.originalTransactionId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(originalTransactionId);
-    }
+/**
+ * Marker interface for S-12 Commands.
+ */
+public interface S12Command {
 }

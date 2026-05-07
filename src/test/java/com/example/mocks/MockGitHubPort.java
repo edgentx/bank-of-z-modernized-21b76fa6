0f17ab901234d71647f91eeabdd3a3c55cd327c2
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 13:30:47 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubPort.java

--- src/test/java/com/example/mocks/MockGitHubPort.java
+++ src/test/java/com/example/mocks/MockGitHubPort.java
@@ -4,26 +4,24 @@ import com.example.ports.GitHubPort;
 
 /**
  * Mock implementation of GitHubPort for testing.
+ * Can simulate successful issue creation or return specific URLs.
  */
 public class MockGitHubPort implements GitHubPort {
 
-    private String issueUrl = "https://github.com/example/repo/issues/1";
-    private boolean shouldFail = false;
+    private String issueUrlToReturn = "https://github.com/example/bank-of-z/issues/1";
+    private boolean createIssueCalled = false;
 
     @Override
-    public String createIssue(String title, String description) {
-        if (shouldFail) {
-            throw new RuntimeException("GitHub API Error");
-        }
-        // Return a deterministic URL based on input or static mock
-        return issueUrl;
+    public String createIssue(String title, String body) {
+        createIssueCalled = true;
+        return issueUrlToReturn;
     }
 
-    public void setIssueUrl(String url) {
-        this.issueUrl = url;
+    public void setIssueUrlToReturn(String url) {
+        this.issueUrlToReturn = url;
     }
 
-    public void setShouldFail(boolean fail) {
-        this.shouldFail = fail;
+    public boolean isCreateIssueCalled() {
+        return createIssueCalled;
     }
 }

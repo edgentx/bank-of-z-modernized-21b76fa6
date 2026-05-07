6abd762a238c88979bbac322ed88da0a0ee29eff
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 15:00:21 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,15 +1,21 @@
 package com.example;
 
+import com.example.adapters.SlackNotificationAdapter;
+import com.example.domain.notification.ReportDefectHandler;
+import com.example.ports.SlackNotificationPort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-/**
- * Main Application Entry Point.
- */
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ReportDefectHandler reportDefectHandler(SlackNotificationPort slackNotificationPort) {
+        return new ReportDefectHandler(slackNotificationPort);
     }
 }

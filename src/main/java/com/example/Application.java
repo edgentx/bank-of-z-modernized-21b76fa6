38f9c0dd23c547d9c063645314f879b4cf0bdd4f
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:42:52 -0500

    feat(feature/s-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,8 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application entry point for Bank of Z Modernized.
+ */
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

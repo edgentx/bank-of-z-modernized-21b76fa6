4175ab620c239e207dc87eb1f051c8406261220f
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:28:19 -0500

    feat(feature/s-10): src/test/java/com/example/runners/CucumberTestSuite.java

--- src/test/java/com/example/runners/CucumberTestSuite.java
+++ src/test/java/com/example/runners/CucumberTestSuite.java
@@ -1,6 +1,5 @@
 package com.example.runners;
 
-import org.junit.platform.suite.api.Configuration;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -8,9 +7,5 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@Configuration(
-    // Ensure strict cucumber matching, though usually defaults are fine
-    plugin = {"pretty", "html:target/cucumber-report", "json:target/cucumber.json"}
-)
 public class CucumberTestSuite {
 }

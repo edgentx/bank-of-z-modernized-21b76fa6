860ffc3a611d23feb3e175f09055c2cc8e7e02de
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 06:48:58 -0500

    feat(feature/s-12): src/test/java/com/example/runners/CucumberTestSuite.java

---
+++ src/test/java/com/example/runners/CucumberTestSuite.java
@@ -0,0 +1,16 @@
+package com.example.runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+public class CucumberTestSuite {
+    // This class acts as the JUnit 5 Suite runner for Cucumber
+}

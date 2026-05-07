89c0737ff94b0c81be8f95c88e80294e6e95f62e
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 05:46:17 -0500

    feat(feature/s-1): src/main/java/com/example/domain/customer/repository/CustomerRepository.java

--- src/main/java/com/example/domain/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/domain/customer/repository/CustomerRepository.java
@@ -1,8 +1,8 @@
 package com.example.domain.customer.repository;
 
 import com.example.domain.customer.model.CustomerAggregate;
-import java.util.Optional;
+
 public interface CustomerRepository {
-    void save(CustomerAggregate aggregate);
-    Optional<CustomerAggregate> findById(String id);
+    CustomerAggregate save(CustomerAggregate aggregate);
+    CustomerAggregate findById(String id);
 }

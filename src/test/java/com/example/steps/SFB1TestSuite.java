fd5b19590b577f4001a21e940c7ccde0ac4ff784
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 17:18:47 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,11 +1,15 @@
 package com.example.steps;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasses({SFB1DefectReportingSteps.class})
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@SuiteDisplayName("S-FB-1: GitHub URL Validation Suite")
 public class SFB1TestSuite {
+    // This class acts as the test runner for the feature file
 }

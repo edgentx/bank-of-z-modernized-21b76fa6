ff78538b864d631c7f476aeb330a772ab981ef67
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:14:53 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/NotificationPort.java

--- src/main/java/com/example/ports/NotificationPort.java
+++ src/main/java/com/example/ports/NotificationPort.java
@@ -2,6 +2,10 @@ package com.example.ports;
 
 import com.example.domain.notification.model.NotificationAggregate;
 
+/**
+ * Port interface for sending notifications.
+ * Implementations will handle the specifics of Slack, Email, etc.
+ */
 public interface NotificationPort {
     void send(NotificationAggregate notification);
 }

381872f1282cd6179f96b3b651f7a218506798de
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:24:54 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,18 +1,16 @@
 package com.example.steps;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
-/**
- * Test Suite for S-18.
- * Executes the Gherkin features defined in S-18.feature.
- */
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S18TestSuite {
-    // This empty class is used by JUnit 5 to run the Cucumber suite
+    // This class acts as the JUnit Suite runner for Cucumber
 }

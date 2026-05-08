1a8d943cab80ac104e27afc66f032af3760001ed
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:54:12 -0500

    feat(feature/s-fb-1): src/main/java/com/example/adapters/config/RestTemplateConfig.java

--- src/main/java/com/example/adapters/config/RestTemplateConfig.java
+++ src/main/java/com/example/adapters/config/RestTemplateConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration for HTTP clients.
+ */
 @Configuration
 public class RestTemplateConfig {
 

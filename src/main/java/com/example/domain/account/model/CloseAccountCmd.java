efe48f99dc7d707cae3c774a5528ef6669085a52
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:51:07 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/CloseAccountCmd.java

--- src/main/java/com/example/domain/account/model/CloseAccountCmd.java
+++ src/main/java/com/example/domain/account/model/CloseAccountCmd.java
@@ -2,9 +2,4 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-import java.math.BigDecimal;
-
-/**
- * Command to close an account.
- */
 public record CloseAccountCmd(String accountNumber) implements Command {}

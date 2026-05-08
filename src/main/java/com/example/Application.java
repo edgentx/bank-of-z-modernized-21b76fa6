647212034b9056ae68d1c63c3f3e767fad88d2bc
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:49:44 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,10 +2,26 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.example.ports.SlackPort;
+import com.example.ports.GitHubPort;
+import com.example.adapters.SlackAdapter;
+import com.example.adapters.GitHubAdapter;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public SlackPort slackPort() {
+        return new SlackAdapter();
+    }
+
+    @Bean
+    public GitHubPort githubPort() {
+        return new GitHubAdapter();
     }
 }

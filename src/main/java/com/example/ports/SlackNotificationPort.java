45932e37558e4ab3abd6c8bdb05965dce121b960
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 03:21:12 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/SlackNotificationPort.java

--- src/main/java/com/example/ports/SlackNotificationPort.java
+++ src/main/java/com/example/ports/SlackNotificationPort.java
@@ -1,16 +1,15 @@
 package com.example.ports;
 
 /**
- * Port interface for sending notifications to Slack.
- * Used to decouple the application logic from the specific Slack client implementation.
+ * Port for sending notifications to Slack.
+ * Used by the Validation Workflow to report defects.
  */
 public interface SlackNotificationPort {
 
     /**
      * Posts a message to the configured Slack channel.
      *
-     * @param messageBody The content of the message to be sent.
-     * @throws IllegalArgumentException if the messageBody is null or empty.
+     * @param messageBody The formatted string to be sent as the message body.
      */
     void postMessage(String messageBody);
 }

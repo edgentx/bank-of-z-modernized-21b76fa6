64c96fca950c2892cbe2e437b96dbe2425e6ff35
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:39:13 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,22 @@
 package com.example;
 
+import com.example.adapters.SlackNotificationAdapter;
+import com.example.ports.SlackNotificationPort;
+import com.example.domain.vforce360.service.DefectReportService;
+import com.slack.api.Slack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Slack slackApiClient() {
+        return Slack.getInstance();
     }
 }

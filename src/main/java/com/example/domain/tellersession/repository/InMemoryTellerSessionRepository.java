c9c89b80ef702c25f557f67842864de61ea98c85
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:26:50 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java

--- src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java
+++ src/main/java/com/example/domain/tellersession/repository/InMemoryTellerSessionRepository.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Optional;
 
 public class InMemoryTellerSessionRepository implements TellerSessionRepository {
-
     private final Map<String, TellerSessionAggregate> store = new HashMap<>();
 
     @Override

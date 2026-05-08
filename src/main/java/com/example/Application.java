4f802d33c2477462c5e38fd964a666e7cef2a5fb
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:34:17 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,19 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Main Entry Point for Bank of Z Application.
- * S-10/S-11/S-12/S-13/S-14/S-15/S-17/S-FB-1
+ * Main Spring Boot Application class.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {
-    "com.example.domain",
-    "com.example.service",
-    "com.example.ports",
-    "com.example.adapters"
-})
 public class Application {
 
     public static void main(String[] args) {

9253e6595feacd8e6b28d9d178d1c94725c8defa
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:49:07 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/VForce360RepositoryPort.java

--- src/main/java/com/example/ports/VForce360RepositoryPort.java
+++ src/main/java/com/example/ports/VForce360RepositoryPort.java
@@ -2,10 +2,12 @@ package com.example.ports;
 
 import com.example.domain.vforce360.model.VForce360Aggregate;
 
+import java.util.Optional;
+
 /**
- * Repository Port for VForce360 Aggregate.
+ * Port interface for VForce360 Repository.
  */
 public interface VForce360RepositoryPort {
-    VForce360Aggregate load(String id);
-    void save(VForce360Aggregate aggregate);
+    VForce360Aggregate save(VForce360Aggregate aggregate);
+    Optional<VForce360Aggregate> findById(String id);
 }

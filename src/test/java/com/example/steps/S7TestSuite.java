3ef400f840c19bdfc5b6badae05d2e95d8d0716c
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:53:55 -0500

    feat(feature/s-7): src/test/java/com/example/steps/S7TestSuite.java

--- src/test/java/com/example/steps/S7TestSuite.java
+++ src/test/java/com/example/steps/S7TestSuite.java
@@ -6,6 +6,6 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-7.feature")
 public class S7TestSuite {
 }

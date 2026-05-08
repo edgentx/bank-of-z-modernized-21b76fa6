794ccc612ea232a6cb233d502a9a5aadfd7c32ee
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 10:46:41 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1RunCucumberTest.java

--- src/test/java/com/example/steps/SFB1RunCucumberTest.java
+++ src/test/java/com/example/steps/SFB1RunCucumberTest.java
@@ -1,18 +1,17 @@
 package com.example.steps;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 /**
- * JUnit Platform wrapper for Cucumber tests.
+ * JUnit Platform wrapper for Cucumber tests specific to S-FB-1.
+ * Enables `mvn test` or `gradle test` to pick up the Gherkin feature.
  */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@ConfigurationParameter(key = Cucumber.GLUE_PROPERTY_NAME, value = "com.example.steps")
+@ConfigurationParameter(key = Cucumber.FEATURE_PROPERTY_NAME, value = "features/S-FB-1.feature")
 public class SFB1RunCucumberTest {
 }

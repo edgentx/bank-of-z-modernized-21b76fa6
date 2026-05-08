a9230088c15646e0061722268bbd4b87345d1a1d
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:32:47 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/CucumberTestSuite.java

--- src/test/java/com/example/steps/CucumberTestSuite.java
+++ src/test/java/com/example/steps/CucumberTestSuite.java
@@ -1,14 +1,14 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Master Cucumber Test Suite.
+ * This configuration is standard for Spring Boot + JUnit 5 + Cucumber.
+ */
 @Suite
-@SelectClasses({
-    S10TestSuite.class,
-    S17TestSuite.class,
-    VW454TestSuite.class // Added regression suite
-})
-public class CucumberTestSuite {
-    // Master Suite
-}
+@IncludeEngines("cucumber")
+@SelectClasses({S10Steps.class, S17Steps.class, SFB1Steps.class})
+public class CucumberTestSuite {}

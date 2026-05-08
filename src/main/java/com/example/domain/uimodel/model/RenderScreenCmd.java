5013a4b26524e1f2d4fb3ecbf619808c417ed324
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:23:58 -0500

    feat(feature/s-21): src/main/java/com/example/domain/uimodel/model/RenderScreenCmd.java

--- src/main/java/com/example/domain/uimodel/model/RenderScreenCmd.java
+++ src/main/java/com/example/domain/uimodel/model/RenderScreenCmd.java
@@ -2,4 +2,5 @@ package com.example.domain.uimodel.model;
 
 import com.example.domain.shared.Command;
 
-public record RenderScreenCmd(String screenId, DeviceType deviceType, int requestedWidth, int requestedDepth) implements Command {}
+public record RenderScreenCmd(String aggregateId, String screenId, String deviceType) implements Command {
+}

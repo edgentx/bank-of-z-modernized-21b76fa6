e44b41a5cf55fe9714b56a0d1109db2de06bacb5
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 03:24:03 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/validation/repository/ValidationRepository.java

--- src/main/java/com/example/domain/validation/repository/ValidationRepository.java
+++ src/main/java/com/example/domain/validation/repository/ValidationRepository.java
@@ -1,9 +1,11 @@
 package com.example.domain.validation.repository;
 
 import com.example.domain.validation.model.ValidationAggregate;
-import java.util.Optional;
 
+/**
+ * Repository for Validation Aggregates.
+ */
 public interface ValidationRepository {
     void save(ValidationAggregate aggregate);
-    Optional<ValidationAggregate> findById(String id);
+    ValidationAggregate find(String validationId);
 }

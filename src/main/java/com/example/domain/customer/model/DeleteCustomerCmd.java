58fa23e0a461fc16860e3aa6870b6cd8469f42a4
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 16:48:23 -0500

    feat(feature/s-4): src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java

--- src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java
+++ src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java
@@ -2,4 +2,4 @@ package com.example.domain.customer.model;
 
 import com.example.domain.shared.Command;
 
-public record DeleteCustomerCmd(String customerId) implements Command {}
+public record DeleteCustomerCmd(String customerId, boolean hasActiveAccounts) implements Command {}

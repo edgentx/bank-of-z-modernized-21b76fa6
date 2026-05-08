d4860ac6d9ef3d9cd3ef02e9f3f3541bab83e14e
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:59:21 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -4,12 +4,16 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+/**
+ * Test Runner for S-FB-1.
+ * Connects the Step Definitions to the Feature File.
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {"features/S-FB-1.feature"},
+    features = "features/S-FB-1.feature",
     glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber-report/S-FB-1.html"}
+    plugin = {"pretty", "html:target/cucumber/S-FB-1.html"}
 )
 public class SFB1TestSuite {
-    // JUnit Runner for Cucumber
+    // Test execution entry point
 }

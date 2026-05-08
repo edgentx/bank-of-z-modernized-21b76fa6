35541877a80e037e264f12c5c9367528c9044e2e
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:21:36 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/repository/VForce360Repository.java

--- src/main/java/com/example/domain/vforce360/repository/VForce360Repository.java
+++ src/main/java/com/example/domain/vforce360/repository/VForce360Repository.java
@@ -2,9 +2,7 @@ package com.example.domain.vforce360.repository;
 
 import com.example.domain.vforce360.model.VForce360Aggregate;
 
-import java.util.Optional;
-
 public interface VForce360Repository {
-    Optional<VForce360Aggregate> findById(String id);
-    void save(VForce360Aggregate aggregate);
+    VForce360Aggregate save(VForce360Aggregate aggregate);
+    VForce360Aggregate findById(String id);
 }

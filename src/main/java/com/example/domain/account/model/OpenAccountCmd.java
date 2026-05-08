03ff8a9a1598bdd808ad96aca3612303b4dc808a
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:04:24 -0500

    feat(feature/s-5): src/main/java/com/example/domain/account/model/OpenAccountCmd.java

--- src/main/java/com/example/domain/account/model/OpenAccountCmd.java
+++ src/main/java/com/example/domain/account/model/OpenAccountCmd.java
@@ -1,8 +1,12 @@
 package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
-
 import java.math.BigDecimal;
 
-public record OpenAccountCmd(String accountId, String customerId, String accountType, BigDecimal initialDeposit, String sortCode) implements Command {
-}
+public record OpenAccountCmd(
+    String accountId,
+    String customerId,
+    String accountType,
+    BigDecimal initialDeposit,
+    String sortCode
+) implements Command {}

a42134ad2cfa61c94316fed5e87c3852c4b10422
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 10:31:10 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubPort.java

--- src/test/java/com/example/mocks/MockGitHubPort.java
+++ src/test/java/com/example/mocks/MockGitHubPort.java
@@ -2,27 +2,26 @@ package com.example.mocks;
 
 import com.example.ports.GitHubPort;
 
-import java.util.Optional;
-
 /**
  * Mock implementation of GitHubPort for testing.
  */
 public class MockGitHubPort implements GitHubPort {
 
-    private String nextIssueUrl = "https://github.com/mock-repo/issues/1";
+    private String mockIssueUrl = "https://github.com/example/repo/issues/1";
+    private String mockBaseUrl = "https://github.com/example/repo";
 
     @Override
     public String createIssue(String title, String body) {
-        // In a real test, we might capture the title/body to verify inputs
-        return nextIssueUrl;
+        // Simulate successful creation
+        return mockIssueUrl;
     }
 
     @Override
-    public Optional<String> getIssueUrl(String issueId) {
-        return Optional.of(nextIssueUrl);
+    public String getRepositoryUrl() {
+        return mockBaseUrl;
     }
 
-    public void setNextIssueUrl(String url) {
-        this.nextIssueUrl = url;
+    public void setMockIssueUrl(String url) {
+        this.mockIssueUrl = url;
     }
 }

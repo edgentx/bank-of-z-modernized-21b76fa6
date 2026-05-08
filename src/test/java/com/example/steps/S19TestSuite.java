3eb02a98a2f9f766f8963b44dbcd842e71cf8d31
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:11:22 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -1,11 +1,9 @@
+package com.example.steps;
+
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-19 BDD Scenarios.
- * Run with: mvn test -Dtest=S19TestSuite
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-19.feature")

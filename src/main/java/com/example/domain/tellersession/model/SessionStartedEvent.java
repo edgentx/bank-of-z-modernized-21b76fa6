1a406575272763777abd682515d822ec0503f2e3
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 10:43:23 -0500

    feat(feature/s-18): src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java

--- src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java
+++ src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java
@@ -5,9 +5,6 @@ import com.example.domain.shared.DomainEvent;
 import java.time.Instant;
 import java.util.UUID;
 
-/**
- * Event published when a Teller Session is successfully started.
- */
 public record SessionStartedEvent(
         String aggregateId,
         String tellerId,

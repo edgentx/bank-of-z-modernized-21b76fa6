f56705280c5861e9765e57769ba16b15b2eeb4f7
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 20:00:46 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubPort.java

--- src/test/java/com/example/mocks/MockGitHubPort.java
+++ src/test/java/com/example/mocks/MockGitHubPort.java
@@ -2,24 +2,23 @@ package com.example.mocks;
 
 import com.example.ports.GitHubPort;
 
-/**
- * Mock implementation of GitHubPort for testing.
- */
 public class MockGitHubPort implements GitHubPort {
-    private String mockUrl = "https://github.com/example/issues/1";
-    private boolean called = false;
+    private String issueUrl = "https://github.com/example/issues/1";
+    private boolean shouldFail = false;
+
+    public void setIssueUrl(String url) {
+        this.issueUrl = url;
+    }
+
+    public void setShouldFail(boolean fail) {
+        this.shouldFail = fail;
+    }
 
     @Override
-    public String createIssue(String summary, String description) {
-        this.called = true;
-        return mockUrl;
-    }
-
-    public void setMockUrl(String url) {
-        this.mockUrl = url;
-    }
-
-    public boolean wasCalled() {
-        return called;
+    public String createIssue(String title, String description, String component) {
+        if (shouldFail) {
+            throw new RuntimeException("GitHub API Error");
+        }
+        return issueUrl;
     }
 }

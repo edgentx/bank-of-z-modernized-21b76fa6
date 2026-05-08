c3ec41b6861f9b5e749047b48cbe36eda13ee98e
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:54:03 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -4,10 +4,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite to run Cucumber scenarios for S-20.
- * S-20: Implement EndSessionCmd on TellerSession
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-20.feature")

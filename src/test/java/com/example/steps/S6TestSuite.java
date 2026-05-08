b5a86d4d123728492c42132f1b39d3b09481eabd
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:42:43 -0500

    feat(feature/s-6): src/test/java/com/example/steps/S6TestSuite.java

--- src/test/java/com/example/steps/S6TestSuite.java
+++ src/test/java/com/example/steps/S6TestSuite.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-6.feature")
+@SelectClasspathResource("features")
 public class S6TestSuite {
+    // JUnit 5 Suite to run Cucumber features
 }

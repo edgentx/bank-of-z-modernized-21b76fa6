7f3f7f9856f38d72269371f4cf62482ecca8facd
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 02:00:19 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,16 +1,15 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features/VW-454.feature"},
+        glue = {"com.example.steps"},
+        plugin = {"pretty", "summary"}
+)
 public class VW454TestSuite {
-    // This class runs the Cucumber features
+    // Test Suite runner for VW-454
 }

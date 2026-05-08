098f9c8d483ee1a269a0596f301e321ccd277103
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:15:09 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -4,12 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite runner for S-20 Features.
- * Run this class to execute the Gherkin scenarios.
- */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-20.feature")
+@SelectClasspathResource("features")
 public class S20TestSuite {
 }

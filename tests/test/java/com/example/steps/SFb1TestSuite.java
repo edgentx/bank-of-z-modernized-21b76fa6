5f2ad4b00cb5f6384b85909e62cf033e3d7c8101
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:37:14 -0500

    feat(feature/s-fb-1): tests/test/java/com/example/steps/SFb1TestSuite.java

---
+++ tests/test/java/com/example/steps/SFb1TestSuite.java
@@ -0,0 +1,10 @@
+package com.example.steps;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
+
+@Test
+@CucumberOptions(features = "src/test/resources/features", glue = "com.example.steps")
+public class SFb1TestSuite extends AbstractTestNGCucumberTests {
+}

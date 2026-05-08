3de6aeba3fb1f1e216f4c5602e40ee2f127ed81e
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:58:13 -0500

    feat(feature/s-19): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,13 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableMongoRepositories
+@ComponentScan(basePackages = "com.example")
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

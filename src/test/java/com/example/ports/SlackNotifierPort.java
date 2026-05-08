46058f17828196b1c792b867c67f325c4a635b02
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 08:16:01 -0500

    feat(feature/s-fb-1): src/test/java/com/example/ports/SlackNotifierPort.java

--- src/test/java/com/example/ports/SlackNotifierPort.java
+++ src/test/java/com/example/ports/SlackNotifierPort.java
@@ -2,6 +2,7 @@ package com.example.ports;
 
 /**
  * Port interface for sending notifications to Slack.
+ * Implemented by adapters (e.g., real HTTP client or test mock).
  */
 public interface SlackNotifierPort {
     void notify(String body);

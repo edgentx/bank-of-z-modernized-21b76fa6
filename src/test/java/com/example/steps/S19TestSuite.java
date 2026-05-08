bc2e3a252639f13ae9faac8e51892057e9e268d1
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:37:07 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -1,15 +1,13 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for Story S-19 (TellerSession Navigation).
- * Cucumber JUnit Platform Engine configuration.
- */
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-19.feature")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S19TestSuite {
 }

0b236e3ea270b98943a8ad6800e463ade7917a99
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:47:07 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,14 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.temporal.spring.boot.TemporalOptionsCustomizer;
-import io.temporal.client.WorkflowClientOptions;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }

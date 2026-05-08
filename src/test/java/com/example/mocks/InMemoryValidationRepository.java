e548c7f0a8784011931d273198c8bb1a4b6f4eb7
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:50:09 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryValidationRepository.java

--- src/test/java/com/example/mocks/InMemoryValidationRepository.java
+++ src/test/java/com/example/mocks/InMemoryValidationRepository.java
@@ -2,20 +2,21 @@ package com.example.mocks;
 
 import com.example.domain.validation.model.ValidationAggregate;
 import com.example.domain.validation.repository.ValidationRepository;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class InMemoryValidationRepository implements ValidationRepository {
     private final Map<String, ValidationAggregate> store = new HashMap<>();
 
     @Override
-    public void save(ValidationAggregate aggregate) {
+    public ValidationAggregate save(ValidationAggregate aggregate) {
         store.put(aggregate.id(), aggregate);
+        return aggregate;
     }
 
     @Override
-    public Optional<ValidationAggregate> findById(String id) {
-        return Optional.ofNullable(store.get(id));
+    public ValidationAggregate findById(String id) {
+        return store.get(id);
     }
 }

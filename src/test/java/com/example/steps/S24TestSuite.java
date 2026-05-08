60b9a1eb3388fbd7f248fa20ef69b0ac7fce9263
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:44:55 -0500

    feat(feature/s-24): src/test/java/com/example/steps/S24TestSuite.java

--- src/test/java/com/example/steps/S24TestSuite.java
+++ src/test/java/com/example/steps/S24TestSuite.java
@@ -4,11 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-24 Cucumber Tests.
- */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-24.feature")
+@SelectClasspathResource("features")
 public class S24TestSuite {
 }

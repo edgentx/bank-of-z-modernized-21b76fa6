526f4d3d63e2e91ecb38eff5d14823f1efa298b1
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 20:22:14 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -4,13 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * JUnit 5 Suite wrapper for running Cucumber tests for S-19.
- * Annotated to discover features in the classpath.
- */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-19.feature")
+@SelectClasspathResource("features")
 public class S19TestSuite {
-    // This empty class is used as a hook for the JUnit Platform Suite Engine
 }

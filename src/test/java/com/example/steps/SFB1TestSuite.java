56f07440497c031edce22865678135e4796e3f35
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 04:56:21 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,15 +1,16 @@
 package com.example.steps;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = {"features/S-FB-1.feature"},
-    glue = {"com.example.steps"},
-    plugin = {"pretty", "summary"},
-    monochrome = true
-)
+/**
+ * Test Suite for S-FB-1.
+ * Aggregates all steps related to Validating VW-454.
+ */
+@Suite
+@SelectClasses({
+    SFB1Steps.class
+})
 public class SFB1TestSuite {
+    // This class is empty, it serves only as a holder for the above annotation.
 }

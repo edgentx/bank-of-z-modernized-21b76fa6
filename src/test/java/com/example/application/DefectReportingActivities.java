47e47a8d5561f1df40a0f8c2186c3c04f7a98fbf
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:49:13 -0500

    feat(feature/s-fb-1): src/test/java/com/example/application/DefectReportingActivities.java

--- src/test/java/com/example/application/DefectReportingActivities.java
+++ src/test/java/com/example/application/DefectReportingActivities.java
@@ -5,7 +5,6 @@ import io.temporal.activity.ActivityMethod;
 
 /**
  * Activity Interface definition for Temporal.
- * Kept separate to allow the Impl class to be defined inline or separately.
  */
 @ActivityInterface
 public interface DefectReportingActivities {

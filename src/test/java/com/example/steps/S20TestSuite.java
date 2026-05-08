b4bb39bdfc45548a47d88351d4f9d2b77d04bca1
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:28:08 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -4,10 +4,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-20 BDD Scenarios.
- * Run with: mvn test -Dtest=S20TestSuite
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-20.feature")

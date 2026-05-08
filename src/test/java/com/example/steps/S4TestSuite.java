fec684271e57ea4e7ec3564bf4dddd402e5671f8
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:56:56 -0500

    feat(feature/s-4): src/test/java/com/example/steps/S4TestSuite.java

--- src/test/java/com/example/steps/S4TestSuite.java
+++ src/test/java/com/example/steps/S4TestSuite.java
@@ -4,8 +4,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite for S-4 Feature.
+ * Runs the Cucumber scenarios defined in features/S-4.feature.
+ */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-4.feature")
 public class S4TestSuite {
 }

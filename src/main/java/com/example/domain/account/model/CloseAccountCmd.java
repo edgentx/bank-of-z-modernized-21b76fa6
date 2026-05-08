a85fcd66cb52a5bce80de38674da12519acdeee3
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:56:56 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/CloseAccountCmd.java

--- src/main/java/com/example/domain/account/model/CloseAccountCmd.java
+++ src/main/java/com/example/domain/account/model/CloseAccountCmd.java
@@ -2,4 +2,8 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-public record CloseAccountCmd(String accountNumber) implements Command {}
+/**
+ * Command to close an account.
+ * The accountId must match the aggregate ID.
+ */
+public record CloseAccountCmd(String accountId) implements Command {}

ab8e7b855fff90c24b7b924cb38dda52cef2bbbc
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:47:40 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryVForce360Repository.java

--- src/test/java/com/example/mocks/InMemoryVForce360Repository.java
+++ src/test/java/com/example/mocks/InMemoryVForce360Repository.java
@@ -2,21 +2,21 @@ package com.example.mocks;
 
 import com.example.domain.vforce360.model.VForce360Aggregate;
 import com.example.domain.vforce360.repository.VForce360Repository;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class InMemoryVForce360Repository implements VForce360Repository {
     private final Map<String, VForce360Aggregate> store = new HashMap<>();
 
     @Override
-    public VForce360Aggregate load(String id) {
-        // Return existing or create new aggregate instance for the workflow
-        return store.getOrDefault(id, new VForce360Aggregate(id));
+    public VForce360Aggregate save(VForce360Aggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
     }
 
     @Override
-    public void save(VForce360Aggregate aggregate) {
-        store.put(aggregate.id(), aggregate);
+    public Optional<VForce360Aggregate> findById(String id) {
+        return Optional.ofNullable(store.get(id));
     }
 }

22a9e62f493aa4bee07c38a353dd23e7140a2549
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:01:13 -0500

    feat(feature/s-6): src/test/java/com/example/steps/S6TestSuite.java

--- src/test/java/com/example/steps/S6TestSuite.java
+++ src/test/java/com/example/steps/S6TestSuite.java
@@ -1,14 +1,15 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for running S-6 Cucumber features.
- */
+import static io.cucumber.junit.platform.engine.CucumberJUnitPlatformRunnerOptions.GLUE;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-6.feature")
+@ConfigurationParameter(key = GLUE, value = "com.example.steps")
 public class S6TestSuite {
 }

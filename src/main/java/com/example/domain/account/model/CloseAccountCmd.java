4c8f87f70b5c591f9a6f1d4956e22ded2ba93ba6
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 18:06:45 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/CloseAccountCmd.java

--- src/main/java/com/example/domain/account/model/CloseAccountCmd.java
+++ src/main/java/com/example/domain/account/model/CloseAccountCmd.java
@@ -2,5 +2,11 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
+import java.util.Objects;
+
 public record CloseAccountCmd(String accountNumber) implements Command {
+    public CloseAccountCmd {
+        Objects.requireNonNull(accountNumber, "accountNumber cannot be null");
+        if (accountNumber.isBlank()) throw new IllegalArgumentException("accountNumber cannot be blank");
+    }
 }

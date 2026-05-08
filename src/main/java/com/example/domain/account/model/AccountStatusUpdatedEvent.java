eebbe76ed2d517cf344d4ad1662be17af6a02439
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:30:41 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/AccountStatusUpdatedEvent.java

--- src/main/java/com/example/domain/account/model/AccountStatusUpdatedEvent.java
+++ src/main/java/com/example/domain/account/model/AccountStatusUpdatedEvent.java
@@ -3,16 +3,13 @@ package com.example.domain.account.model;
 import com.example.domain.shared.DomainEvent;
 
 import java.time.Instant;
-import java.util.UUID;
 
 public record AccountStatusUpdatedEvent(
         String aggregateId,
-        String accountNumber,
         AccountAggregate.AccountStatus oldStatus,
         AccountAggregate.AccountStatus newStatus,
         Instant occurredAt
 ) implements DomainEvent {
-
     @Override
     public String type() {
         return "account.status.updated";

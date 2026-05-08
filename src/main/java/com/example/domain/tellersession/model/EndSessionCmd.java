2651f767e396a6f075c6fb509c9339dd0d1b8112
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:07:45 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/model/EndSessionCmd.java

--- src/main/java/com/example/domain/tellersession/model/EndSessionCmd.java
+++ src/main/java/com/example/domain/tellersession/model/EndSessionCmd.java
@@ -2,4 +2,15 @@ package com.example.domain.tellersession.model;
 
 import com.example.domain.shared.Command;
 
-public record EndSessionCmd(String sessionId) implements Command {}
+import java.time.Instant;
+import java.util.Objects;
+
+/**
+ * Command to terminate a Teller Session.
+ */
+public record EndSessionCmd(String sessionId, Instant occurredAt) implements Command {
+    public EndSessionCmd {
+        Objects.requireNonNull(sessionId, "sessionId cannot be null");
+        Objects.requireNonNull(occurredAt, "occurredAt cannot be null");
+    }
+}

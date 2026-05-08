23898aadb3d6a0eae8dc636d5febaab95a0d19e4
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:45:15 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -4,6 +4,9 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite Runner for S-20 Cucumber Tests.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-20.feature")

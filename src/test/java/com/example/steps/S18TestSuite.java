b3e8f2a6b99702a0b570f2a9f69e3ec5a5b168cd
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:27:59 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -4,8 +4,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * JUnit 5 Suite wrapper to run Cucumber features.
+ * This is detected by the Maven Surefire plugin during the test phase.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 public class S18TestSuite {
+    // No code needed here, annotations drive the execution
 }

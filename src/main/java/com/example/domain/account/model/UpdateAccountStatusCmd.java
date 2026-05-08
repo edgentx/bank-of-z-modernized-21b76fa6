f225c2c6569cdd20d87a1ca00819602341acc598
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:42:02 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java

--- src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
+++ src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
@@ -2,13 +2,4 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-import java.util.Optional;
-
-/**
- * Command to update the status of an Account aggregate.
- */
-public record UpdateAccountStatusCmd(
-    String accountId,
-    AccountAggregate.AccountStatus newStatus,
-    String accountNumber // Optional inclusion to check immutability invariant
-) implements Command {}
+public record UpdateAccountStatusCmd(String accountId, AccountStatus newStatus) implements Command {}

334878392c171e0c1e70a7cffd3a072c8d890563
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 08:15:02 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java

--- src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java
+++ src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java
@@ -1,7 +1,6 @@
 package com.example.domain.vforce360.service;
 
 import com.example.domain.defect.model.ReportDefectCmd;
-import com.example.services.DefectReportingService;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 

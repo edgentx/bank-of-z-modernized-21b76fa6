d2f184397c53bdab67b68c3df1f3452decc17964
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:13:18 -0500

    feat(feature/s-fb-1): src/main/java/com/example/workflows/ReportDefectActivity.java

--- src/main/java/com/example/workflows/ReportDefectActivity.java
+++ src/main/java/com/example/workflows/ReportDefectActivity.java
@@ -1,9 +1,5 @@
 package com.example.workflows;
 
-import com.example.domain.defect.model.DefectAggregate;
-import com.example.domain.defect.model.ReportDefectCmd;
-import com.example.domain.defect.repository.DefectRepository;
-import com.example.ports.SlackNotificationPort;
 import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
 

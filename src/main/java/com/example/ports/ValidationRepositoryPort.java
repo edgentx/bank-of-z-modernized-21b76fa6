df1210d7908441aee6e46a0b2f902586bf8cf1a9
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:44:40 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/ValidationRepositoryPort.java

--- src/main/java/com/example/ports/ValidationRepositoryPort.java
+++ src/main/java/com/example/ports/ValidationRepositoryPort.java
@@ -1,12 +1,8 @@
 package com.example.ports;
 
 import com.example.domain.validation.model.ValidationAggregate;
-
 import java.util.Optional;
 
-/**
- * Port interface for Validation Repository.
- */
 public interface ValidationRepositoryPort {
     ValidationAggregate save(ValidationAggregate aggregate);
     Optional<ValidationAggregate> findById(String id);

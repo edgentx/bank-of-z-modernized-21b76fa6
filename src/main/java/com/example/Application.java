348512571273df2ca0fd85b51a1414a64d9b863f
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:37:23 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.java, args);
+        SpringApplication.run(Application.class, args);
     }
 }

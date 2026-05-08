09310a411d9f9cbbb296480768a6eb47d23e649c
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 17:41:18 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/CloseAccountCmd.java

--- src/main/java/com/example/domain/account/model/CloseAccountCmd.java
+++ src/main/java/com/example/domain/account/model/CloseAccountCmd.java
@@ -2,5 +2,5 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-public record CloseAccountCmd(String accountNumber, String accountNumberCheck) implements Command {
+public record CloseAccountCmd(String accountNumber) implements Command {
 }

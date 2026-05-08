57b7e63d4d07cfb595cceff67ccd796a942dabf7
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:58:08 -0500

    feat(feature/s-21): src/main/java/com/example/domain/ui/model/RenderScreenCmd.java

--- src/main/java/com/example/domain/ui/model/RenderScreenCmd.java
+++ src/main/java/com/example/domain/ui/model/RenderScreenCmd.java
@@ -1,11 +1,5 @@
 package com.example.domain.ui.model;
 
 import com.example.domain.shared.Command;
-import java.util.Objects;
 
-public record RenderScreenCmd(String screenId, String deviceType) implements Command {
-    public RenderScreenCmd {
-        Objects.requireNonNull(screenId, "screenId cannot be null");
-        Objects.requireNonNull(deviceType, "deviceType cannot be null");
-    }
-}
+public record RenderScreenCmd(String aggregateId, String screenId, String deviceType) implements Command {}

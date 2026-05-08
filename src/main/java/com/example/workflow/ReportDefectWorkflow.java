82a6ce276d79bbb64543a9e257dc969fc7a3b6a8
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:23:57 -0500

    feat(feature/s-fb-1): src/main/java/com/example/workflow/ReportDefectWorkflow.java

--- src/main/java/com/example/workflow/ReportDefectWorkflow.java
+++ src/main/java/com/example/workflow/ReportDefectWorkflow.java
@@ -1,6 +1,5 @@
 package com.example.workflow;
 
-import com.example.domain.notification.model.NotificationAggregate;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 

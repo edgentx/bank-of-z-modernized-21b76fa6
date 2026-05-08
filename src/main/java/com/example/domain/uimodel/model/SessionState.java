3445a778e0f0ed689aed7595a3ad2b9bf7da03ed
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:26:49 -0500

    feat(feature/s-20): src/main/java/com/example/domain/uimodel/model/SessionState.java

---
+++ src/main/java/com/example/domain/uimodel/model/SessionState.java
@@ -0,0 +1,5 @@
+package com.example.domain.uimodel.model;
+
+// Package public enums or constants if needed, but we defined it inside the Aggregate for S-20 simplicity.
+// However, if the repository or other layers reference it, it should be public here.
+// For now, we rely on TellerSessionAggregate.SessionState to avoid extra file.

52bd9554a30c24ec5dcb9b4ee80255e4f5b2c0c1
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 13:17:02 -0500

    feat(feature/s-22): src/test/java/com/example/domain/screenmap/repository/ScreenMapRepository.java

---
+++ src/test/java/com/example/domain/screenmap/repository/ScreenMapRepository.java
@@ -0,0 +1,10 @@
+package com.example.domain.screenmap.repository;
+
+import com.example.domain.screenmap.model.ScreenMapAggregate;
+
+import java.util.Optional;
+
+public interface ScreenMapRepository {
+    ScreenMapAggregate save(ScreenMapAggregate aggregate);
+    Optional<ScreenMapAggregate> findById(String id);
+}

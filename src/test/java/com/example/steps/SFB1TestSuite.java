3f59dc8d45f5a14a11b2f9f775627ff748a54a80
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:45:49 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,14 +1,15 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 /**
- * Test Suite for S-FB-1.
- * Run this class to execute the Cucumber scenario.
+ * Test Suite configuration for Cucumber tests related to Feature S-FB-1.
  */
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasses(SFB1Steps.class)
 public class SFB1TestSuite {
-    // JUnit 5 Suite wrapper
+    // This class acts as the glue for the JUnit 5 Platform to find the Cucumber tests
 }

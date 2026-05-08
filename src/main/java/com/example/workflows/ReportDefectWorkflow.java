ab225227740390ee564622f38639834829f5cc97
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 20:00:38 -0500

    feat(feature/s-fb-1): src/main/java/com/example/workflows/ReportDefectWorkflow.java

--- src/main/java/com/example/workflows/ReportDefectWorkflow.java
+++ src/main/java/com/example/workflows/ReportDefectWorkflow.java
@@ -3,12 +3,9 @@ package com.example.workflows;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
-/**
- * Temporal Workflow Interface for reporting a defect.
- */
 @WorkflowInterface
 public interface ReportDefectWorkflow {
 
     @WorkflowMethod
-    String execute(String summary, String description, String slackChannel);
+    void reportDefect(String title, String description, String component);
 }

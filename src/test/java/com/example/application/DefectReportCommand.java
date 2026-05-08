8bf4d549dd6569cdfef4179864d10ca23bf3eb22
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:44:36 -0500

    feat(feature/s-fb-1): src/test/java/com/example/application/DefectReportCommand.java

--- src/test/java/com/example/application/DefectReportCommand.java
+++ src/test/java/com/example/application/DefectReportCommand.java
@@ -1,11 +1,13 @@
 package com.example.application;
 
+import java.io.Serializable;
+
 /**
  * Command object representing a defect report request.
- * Immutable data carrier.
+ * Must be Serializable to pass through Temporal workflows.
  */
 public record DefectReportCommand(
     String defectId,
     String title,
     String severity
-) {}
+) implements Serializable {}

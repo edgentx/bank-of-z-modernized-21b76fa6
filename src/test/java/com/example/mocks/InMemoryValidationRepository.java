74d5357287240d903536be9bf98ef9190767d57a
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:51:07 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryValidationRepository.java

--- src/test/java/com/example/mocks/InMemoryValidationRepository.java
+++ src/test/java/com/example/mocks/InMemoryValidationRepository.java
@@ -7,7 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * In-memory implementation of ValidationRepository for testing.
+ */
 public class InMemoryValidationRepository implements ValidationRepository {
+
     private final Map<String, ValidationAggregate> store = new HashMap<>();
 
     @Override

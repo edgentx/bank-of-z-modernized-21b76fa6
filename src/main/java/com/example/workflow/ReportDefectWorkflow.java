1e9d7e154bf348a5ccc39a520377989a862a4970
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:47:38 -0500

    feat(feature/s-fb-1): src/main/java/com/example/workflow/ReportDefectWorkflow.java

--- src/main/java/com/example/workflow/ReportDefectWorkflow.java
+++ src/main/java/com/example/workflow/ReportDefectWorkflow.java
@@ -1,22 +1,10 @@
 package com.example.workflow;
 
-import io.temporal.workflow.SignalMethod;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
-/**
- * Temporal Workflow interface for reporting a defect.
- * Orchestrates the creation of a GitHub issue and notification via Slack.
- */
 @WorkflowInterface
 public interface ReportDefectWorkflow {
-
-    /**
-     * Reports a defect by creating a GitHub Issue and notifying Slack.
-     *
-     * @param defectTitle Title of the defect.
-     * @param defectBody  Body of the defect.
-     */
     @WorkflowMethod
-    void reportDefect(String defectTitle, String defectBody);
+    void reportDefect(String defectId, String description);
 }

5aa31b1b46783043088f70147885c474b478b73e
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:29:49 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockSlackPort.java

--- src/test/java/com/example/mocks/MockSlackPort.java
+++ src/test/java/com/example/mocks/MockSlackPort.java
@@ -1,25 +1,16 @@
 package com.example.mocks;
 
 import com.example.ports.SlackPort;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MockSlackPort implements SlackPort {
-    public final List<String> messages = new ArrayList<>();
-    public String lastChannel;
+    private String lastMessage;
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
 
     @Override
-    public void postMessage(String channel, String text) {
-        this.lastChannel = channel;
-        this.messages.add(text);
-    }
-
-    public boolean containsUrl(String url) {
-        return messages.stream().anyMatch(msg -> msg.contains(url));
-    }
-
-    public void clear() {
-        messages.clear();
-        lastChannel = null;
+    public void sendMessage(String message) {
+        this.lastMessage = message;
     }
 }

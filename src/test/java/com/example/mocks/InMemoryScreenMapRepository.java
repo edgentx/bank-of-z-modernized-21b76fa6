99cd62d63594102a9b80dd22a7bc3ee3a4b7540a
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:17:33 -0500

    feat(feature/s-22): src/test/java/com/example/mocks/InMemoryScreenMapRepository.java

---
+++ src/test/java/com/example/mocks/InMemoryScreenMapRepository.java
@@ -0,0 +1,22 @@
+package com.example.mocks;
+
+import com.example.domain.userinterface.model.ScreenMapAggregate;
+import com.example.domain.userinterface.repository.ScreenMapRepository;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class InMemoryScreenMapRepository implements ScreenMapRepository {
+    private final Map<String, ScreenMapAggregate> store = new HashMap<>();
+
+    @Override
+    public ScreenMapAggregate save(ScreenMapAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
+    }
+
+    @Override
+    public Optional<ScreenMapAggregate> findById(String screenId) {
+        return Optional.ofNullable(store.get(screenId));
+    }
+}

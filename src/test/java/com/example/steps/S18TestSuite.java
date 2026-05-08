53f5a8b834295c367de9690ad4162abcbb7ae3bf
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:41:27 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,15 +1,10 @@
-package com.example.steps;
-
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * JUnit 5 Suite configuration for running Cucumber features.
- * S-18: TellerSession
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-18.feature")
 public class S18TestSuite {
+    // JUnit 5 Suite to run Cucumber features
 }

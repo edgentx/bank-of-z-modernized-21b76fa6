293c5c0e8048c2b51f20a51df3bec52c19064cb4
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:04:18 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/validation/model/ReportDefectCmd.java

--- src/main/java/com/example/domain/validation/model/ReportDefectCmd.java
+++ src/main/java/com/example/domain/validation/model/ReportDefectCmd.java
@@ -2,4 +2,9 @@ package com.example.domain.validation.model;
 
 import com.example.domain.shared.Command;
 
-public record ReportDefectCmd(String validationId, String description) implements Command {}
+public record ReportDefectCmd(
+    String validationId,
+    String severity,
+    String component,
+    String description
+) implements Command {}

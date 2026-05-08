e220327a05b4aae6e063be60104b339e464a57fb
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 14:39:34 -0500

    feat(feature/s-24): src/test/java/com/example/steps/S24TestSuite.java

--- src/test/java/com/example/steps/S24TestSuite.java
+++ src/test/java/com/example/steps/S24TestSuite.java
@@ -9,7 +9,8 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-24.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S24TestSuite {
+    // JUnit 5 Suite to run Cucumber features
 }

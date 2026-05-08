e362fe82d0d2583333e004f7e1a95067410ce1d1
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:35:44 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockNotificationRepository.java

--- src/test/java/com/example/mocks/MockNotificationRepository.java
+++ src/test/java/com/example/mocks/MockNotificationRepository.java
@@ -2,18 +2,17 @@ package com.example.mocks;
 
 import com.example.domain.notification.model.NotificationAggregate;
 import com.example.domain.notification.repository.NotificationRepository;
-
 import java.util.HashMap;
-import java.util.Map;
+    import java.util.Map;
 import java.util.Optional;
 
 public class MockNotificationRepository implements NotificationRepository {
-
     private final Map<String, NotificationAggregate> store = new HashMap<>();
 
     @Override
-    public void save(NotificationAggregate aggregate) {
+    public NotificationAggregate save(NotificationAggregate aggregate) {
         store.put(aggregate.id(), aggregate);
+        return aggregate;
     }
 
     @Override

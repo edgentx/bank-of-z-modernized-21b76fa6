7ad1afb21a4c49110c12bc151cfa2c45b7bc05bc
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:23:49 -0500

    feat(feature/s-fb-1): src/main/java/com/example/workflow/ReportDefectWorkflow.java

--- src/main/java/com/example/workflow/ReportDefectWorkflow.java
+++ src/main/java/com/example/workflow/ReportDefectWorkflow.java
@@ -1,16 +1,11 @@
 package com.example.workflow;
 
 import com.example.domain.notification.model.NotificationAggregate;
-import com.example.ports.NotificationPort;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
-/**
- * Temporal Workflow interface for reporting a defect.
- */
 @WorkflowInterface
 public interface ReportDefectWorkflow {
-
     @WorkflowMethod
-    void reportDefect(String defectId, String description);
+    String reportDefect(String description, String severity);
 }

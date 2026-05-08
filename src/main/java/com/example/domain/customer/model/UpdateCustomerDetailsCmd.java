614154e3849c1a16e9d8a51af6fdabcbff638aaf
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:45:29 -0500

    feat(feature/s-3): src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java

--- src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
+++ src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
@@ -3,14 +3,14 @@ package com.example.domain.customer.model;
 import com.example.domain.shared.Command;
 
 /**
- * Command to update details for an existing Customer.
+ * Command to update customer details.
  */
 public record UpdateCustomerDetailsCmd(
-    String customerId,
-    String emailAddress,
-    String sortCode,
-    String governmentId,
-    String fullName,
-    String dateOfBirth,
-    boolean hasActiveAccounts
+        String customerId,
+        String emailAddress,
+        String sortCode,
+        String fullName,
+        String dateOfBirth,
+        String governmentId,
+        boolean hasActiveBankAccounts // Flag to simulate checking active accounts for the scenario
 ) implements Command {}

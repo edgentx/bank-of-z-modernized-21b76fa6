48c39809ec2360935c0c4977a6376b7ba8f6a6f1
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 08:33:17 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -4,8 +4,14 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * JUnit 5 Suite configuration to run Cucumber features.
+ * <p>
+ * Uses JUnit Platform Suite to discover and run Cucumber scenarios located in
+ * the classpath resource 'features'.
+ */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-18.feature")
+@SelectClasspathResource("features")
 public class S18TestSuite {
 }

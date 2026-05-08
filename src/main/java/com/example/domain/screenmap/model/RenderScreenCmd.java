bfc61aeddd8e194384f4110a2ff5b7a651fc7b9a
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:01:21 -0500

    feat(feature/s-21): src/main/java/com/example/domain/screenmap/model/RenderScreenCmd.java

--- src/main/java/com/example/domain/screenmap/model/RenderScreenCmd.java
+++ src/main/java/com/example/domain/screenmap/model/RenderScreenCmd.java
@@ -2,5 +2,5 @@ package com.example.domain.screenmap.model;
 
 import com.example.domain.shared.Command;
 
-public record RenderScreenCmd(String screenId, String screenDefinition, String deviceType) implements Command {
+public record RenderScreenCmd(String screenId, String deviceType) implements Command {
 }

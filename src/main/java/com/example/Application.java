624d398c7fd7325f58fabf9673385493bcc57b36
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 09:10:08 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,15 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Main entry point for the Bank-of-Z Modernization application.
- */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example"})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

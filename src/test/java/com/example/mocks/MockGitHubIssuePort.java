8e4ef7f006fbfebd6c8d4bab8e5af76ed683ea92
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 09:04:43 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubIssuePort.java

--- src/test/java/com/example/mocks/MockGitHubIssuePort.java
+++ src/test/java/com/example/mocks/MockGitHubIssuePort.java
@@ -1,16 +1,30 @@
 package com.example.mocks;
 
 import com.example.ports.GitHubIssuePort;
+import java.util.Optional;
 
+/**
+ * Mock implementation of GitHubIssuePort for testing.
+ * Allows simulating successful issue creation with specific URLs.
+ */
 public class MockGitHubIssuePort implements GitHubIssuePort {
-    private final String mockUrl;
 
-    public MockGitHubIssuePort(String mockUrl) {
-        this.mockUrl = mockUrl;
+    private String mockUrl = "https://github.com/example/repo/issues/1";
+    private boolean shouldSucceed = true;
+
+    public void setMockUrl(String url) {
+        this.mockUrl = url;
+    }
+
+    public void setShouldSucceed(boolean succeed) {
+        this.shouldSucceed = succeed;
     }
 
     @Override
-    public String createIssue(String title, String description) {
-        return mockUrl;
+    public Optional<String> createIssue(String title, String description) {
+        if (shouldSucceed) {
+            return Optional.of(mockUrl);
+        }
+        return Optional.empty();
     }
 }

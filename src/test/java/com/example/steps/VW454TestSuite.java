6aca3e11f55b74fc35b7aa55b15a5246d0b364e0
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 20:15:07 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,10 +1,19 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-@Suite
-@SelectClasses(VW454Steps.class)
+/**
+ * Test Suite for VW-454 Regression Tests.
+ * Connects the Step Definitions to the Cucumber Runner.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = {"features/S-FB-1.feature"}, // The feature file we need to create
+    glue = {"com.example.steps"},
+    plugin = {"pretty", "summary"}
+)
 public class VW454TestSuite {
-    // JUnit 5 Suite to run the Cucumber/Java steps logic
+    // This class serves as the entry point for the JUnit/Cucumber test runner
 }

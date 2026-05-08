cab3819dd7e052139682385ffcdcabc5d93939be
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 20:58:33 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/model/InvalidNavigationStateException.java

---
+++ src/main/java/com/example/domain/tellersession/model/InvalidNavigationStateException.java
@@ -0,0 +1,7 @@
+package com.example.domain.tellersession.model;
+
+public class InvalidNavigationStateException extends RuntimeException {
+    public InvalidNavigationStateException(String message) {
+        super(message);
+    }
+}

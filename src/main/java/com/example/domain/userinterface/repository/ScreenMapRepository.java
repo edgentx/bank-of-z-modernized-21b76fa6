d3cdc9b3e20eb01b79c4f71a0940df9609f83e7c
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:17:30 -0500

    feat(feature/s-22): src/main/java/com/example/domain/userinterface/repository/ScreenMapRepository.java

---
+++ src/main/java/com/example/domain/userinterface/repository/ScreenMapRepository.java
@@ -0,0 +1,9 @@
+package com.example.domain.userinterface.repository;
+
+import com.example.domain.userinterface.model.ScreenMapAggregate;
+import java.util.Optional;
+
+public interface ScreenMapRepository {
+    ScreenMapAggregate save(ScreenMapAggregate aggregate);
+    Optional<ScreenMapAggregate> findById(String screenId);
+}

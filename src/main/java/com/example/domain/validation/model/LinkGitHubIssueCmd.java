361397765d09a7a324bb38527193f2f1de900251
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:19:15 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/validation/model/LinkGitHubIssueCmd.java

--- src/main/java/com/example/domain/validation/model/LinkGitHubIssueCmd.java
+++ src/main/java/com/example/domain/validation/model/LinkGitHubIssueCmd.java
@@ -2,4 +2,7 @@ package com.example.domain.validation.model;
 
 import com.example.domain.shared.Command;
 
-public record LinkGitHubIssueCmd(String validationId, String issueUrl) implements Command {}
+public record LinkGitHubIssueCmd(
+    String validationId,
+    String url
+) implements Command {}

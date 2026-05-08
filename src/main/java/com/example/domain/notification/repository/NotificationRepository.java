b91fb6593d31189755930a1313ed8b6d539ab2a0
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:04:24 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/notification/repository/NotificationRepository.java

--- src/main/java/com/example/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/domain/notification/repository/NotificationRepository.java
@@ -1,9 +1,7 @@
 package com.example.domain.notification.repository;
 
 import com.example.domain.notification.model.NotificationAggregate;
-import java.util.Optional;
 
 public interface NotificationRepository {
     NotificationAggregate save(NotificationAggregate aggregate);
-    Optional<NotificationAggregate> findById(String id);
 }

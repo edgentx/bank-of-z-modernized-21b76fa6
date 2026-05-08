3c3b35b6f5c81cd0597e93a05f9b17cfddf975fe
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:56:54 -0500

    feat(feature/s-4): src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java

--- src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java
+++ src/main/java/com/example/domain/customer/model/DeleteCustomerCmd.java
@@ -4,5 +4,6 @@ import com.example.domain.shared.Command;
 
 /**
  * Command to delete a customer.
+ * Records the intent to mark a customer as deleted.
  */
 public record DeleteCustomerCmd(String customerId) implements Command {}

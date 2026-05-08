92982e5d49eee19573d46f85b39a55677bb7e947
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:44:35 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,6 +1,5 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameters;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;

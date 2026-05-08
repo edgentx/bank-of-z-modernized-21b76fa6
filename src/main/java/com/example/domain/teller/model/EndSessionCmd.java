666f3a8bcac88ba9c62a472243d8a7b355cb7b2c
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 09:37:02 -0500

    feat(feature/s-20): src/main/java/com/example/domain/teller/model/EndSessionCmd.java

--- src/main/java/com/example/domain/teller/model/EndSessionCmd.java
+++ src/main/java/com/example/domain/teller/model/EndSessionCmd.java
@@ -2,4 +2,7 @@ package com.example.domain.teller.model;
 
 import com.example.domain.shared.Command;
 
+/**
+ * Command to end a TellerSession.
+ */
 public record EndSessionCmd(String sessionId) implements Command {}

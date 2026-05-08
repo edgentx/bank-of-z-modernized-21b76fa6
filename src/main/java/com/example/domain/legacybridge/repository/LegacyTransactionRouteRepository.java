00a43b99e375db4ad315af51e18f0bdb7f1ce4f7
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 13:20:36 -0500

    feat(feature/s-23): src/main/java/com/example/domain/legacybridge/repository/LegacyTransactionRouteRepository.java

---
+++ src/main/java/com/example/domain/legacybridge/repository/LegacyTransactionRouteRepository.java
@@ -0,0 +1,10 @@
+package com.example.domain.legacybridge.repository;
+
+import com.example.domain.legacybridge.model.LegacyTransactionRouteAggregate;
+
+import java.util.Optional;
+
+public interface LegacyTransactionRouteRepository {
+    void save(LegacyTransactionRouteAggregate aggregate);
+    Optional<LegacyTransactionRouteAggregate> findById(String id);
+}

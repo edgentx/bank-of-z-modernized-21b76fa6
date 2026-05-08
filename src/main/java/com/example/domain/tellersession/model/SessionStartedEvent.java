ae2fc0c49c623721ecb3a68502cb3bdae5431ead
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:25:49 -0500

    feat(feature/s-18): src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java

--- src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java
+++ src/main/java/com/example/domain/tellersession/model/SessionStartedEvent.java
@@ -3,7 +3,6 @@ package com.example.domain.tellersession.model;
 import com.example.domain.shared.DomainEvent;
 
 import java.time.Instant;
-import java.util.UUID;
 
 public record SessionStartedEvent(
     String aggregateId,

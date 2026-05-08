a9e189a3d53b5e2134f557e9d07edc04627b54cc
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:42:30 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.example.domain",
+    "com.example.mocks" // Explicitly allow the mock adapter for this test suite
+})
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }

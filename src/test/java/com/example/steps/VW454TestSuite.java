d026c975fda4c503828341dc45adc4397372ae0f
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 23:06:51 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -4,15 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-/**
- * Test Suite configuration for running VW-454 regression tests.
- */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "../../features/VW-454.feature", 
-    glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber/VW-454.html"}
+        features = "../../features/S-FB-1.feature", 
+        glue = {"com.example.steps"},
+        plugin = {"pretty", "html:target/cucumber-report/S-FB-1.html"}
 )
 public class VW454TestSuite {
-    // This class bootstraps the Cucumber runner
+    // Test runner configuration
 }

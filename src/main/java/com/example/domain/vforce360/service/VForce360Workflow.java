12ec7f68a7c9ebfb82efa50daeb62b1d473e498d
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:46:09 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java

--- src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java
+++ src/main/java/com/example/domain/vforce360/service/VForce360Workflow.java
@@ -2,8 +2,6 @@ package com.example.domain.vforce360.service;
 
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
-import io.temporal.spring.boot.WorkflowImpl;
-import org.springframework.stereotype.Component;
 
 @WorkflowInterface
 public interface VForce360Workflow {

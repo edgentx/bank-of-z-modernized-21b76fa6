9ea2c66fd526e23249b0bf17391a3f308fcf0e4d
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:58:42 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/DefectReportWorkflow.java

--- src/main/java/com/example/domain/vforce360/DefectReportWorkflow.java
+++ src/main/java/com/example/domain/vforce360/DefectReportWorkflow.java
@@ -3,17 +3,8 @@ package com.example.domain.vforce360;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 
-/**
- * Temporal Workflow interface for orchestrating defect reporting.
- */
 @WorkflowInterface
 public interface DefectReportWorkflow {
-
-    /**
-     * Initiates the defect reporting process.
-     *
-     * @param defectId The ID of the defect to report.
-     */
     @WorkflowMethod
-    void reportDefect(String defectId);
+    String reportDefect(String summary, String description);
 }

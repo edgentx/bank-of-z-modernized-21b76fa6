f1b7e0c4afceb11f7ebedf9bd8c2eb1d099f6ae3
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 09:01:43 -0500

    feat(feature/s-21): src/test/java/com/example/steps/S21TestSuite.java

--- src/test/java/com/example/steps/S21TestSuite.java
+++ src/test/java/com/example/steps/S21TestSuite.java
@@ -1,5 +1,6 @@
 package com.example.steps;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -7,5 +8,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-21.feature")
+@ConfigurationParameter(key = "cucumber.junit-platform.naming-strategy", value = "long")
 public class S21TestSuite {
 }

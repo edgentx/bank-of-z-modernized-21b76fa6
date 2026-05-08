865338674bce3cafa771a00a51d66fd0afbff27b
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 21:05:06 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryNotificationRepository.java

---
+++ src/test/java/com/example/mocks/InMemoryNotificationRepository.java
@@ -0,0 +1,22 @@
+package com.example.mocks;
+
+import com.example.domain.notification.model.NotificationAggregate;
+import com.example.domain.notification.repository.NotificationRepository;
+
+import java.util.HashMap;
+    import java.util.Map;
+import java.util.Optional;
+
+public class InMemoryNotificationRepository implements NotificationRepository {
+    private final Map<String, NotificationAggregate> store = new HashMap<>();
+
+    @Override
+    public void save(NotificationAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+    }
+
+    @Override
+    public Optional<NotificationAggregate> findById(String id) {
+        return Optional.ofNullable(store.get(id));
+    }
+}

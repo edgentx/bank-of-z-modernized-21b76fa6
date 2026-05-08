6b0fd556d4305a7090ed519f1b6599f15b202dbc
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:29:27 -0500

    feat(feature/s-18): src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java

--- src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java
+++ src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java
@@ -2,4 +2,7 @@ package com.example.domain.tellersession.model;
 
 import com.example.domain.shared.Command;
 
+/**
+ * Command to initiate a Teller Session.
+ */
 public record StartSessionCmd(String sessionId, String tellerId, String terminalId) implements Command {}

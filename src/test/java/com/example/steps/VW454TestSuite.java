365e6ede074a0463c5b52b855e549c2a8ead35b4
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:33:05 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,17 +1,13 @@
 package com.example.steps;
 
-import io.cucumber.junit.platform.engine.Cucumber;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-/**
- * Test Suite for VW-454 Regression.
- * Located in e2e/regression/ equivalent location via the resource selector.
- */
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features/VW-454.feature") // Assumes feature file is placed in resources/features
-public class VW454TestSuite {
-    // Suite configuration is handled by annotations
-}
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "features/VW-454.feature",
+    glue = "com.example.steps",
+    plugin = {"pretty", "html:target/cucumber-report/VW-454"}
+)
+public class VW454TestSuite {}

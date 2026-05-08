1b2037e9ab88a6ef0c778d5334b7a87475e8d0bf
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:36:15 -0500

    feat(feature/s-3): src/test/java/com/example/steps/S3TestSuite.java

--- src/test/java/com/example/steps/S3TestSuite.java
+++ src/test/java/com/example/steps/S3TestSuite.java
@@ -4,10 +4,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for S-3 Feature (UpdateCustomerDetailsCmd).
- * Run with 'mvn test -Dtest=S3TestSuite' or similar JUnit Platform configuration.
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-3.feature")

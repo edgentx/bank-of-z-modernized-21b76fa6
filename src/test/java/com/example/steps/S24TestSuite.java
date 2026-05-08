214e42989d9fff69cf7d38d7fc39627e5225a58e
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 15:18:26 -0500

    feat(feature/s-24): src/test/java/com/example/steps/S24TestSuite.java

--- src/test/java/com/example/steps/S24TestSuite.java
+++ src/test/java/com/example/steps/S24TestSuite.java
@@ -4,11 +4,8 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for running Cucumber scenarios for Story S-24.
- */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/S-24.feature")
+@SelectClasspathResource("features")
 public class S24TestSuite {
 }

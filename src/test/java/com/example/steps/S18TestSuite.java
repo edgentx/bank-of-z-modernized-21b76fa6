46b8eb15e00352c37372fc04a2a8d739073ce433
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 12:25:50 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,13 +1,7 @@
-package com.example.steps;
-
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * JUnit 5 Suite wrapper for Cucumber S-18 tests.
- * Run this class to execute the feature file.
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-18.feature")

f65b1a3958e74d8299fc56043a939929b881ca7c
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 19:39:52 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,18 +1,14 @@
 package com.example.steps;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite for VW-454 Validation.
- */
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = {"features/S-FB-1.feature"},
-    glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber-report/S-FB-1"}
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/VW-454.feature")
+@Cucumber
 public class VW454TestSuite {
-    // Suite class runs the feature file.
+    // This suite runs the Cucumber feature file for VW-454
 }

2d5c00bfb4bb051eb4b37f30bcc784a9ffdd5f62
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:43:58 -0500

    feat(feature/s-18): src/main/java/com/example/domain/tellersession/model/TellerSessionStartedEvent.java

--- src/main/java/com/example/domain/tellersession/model/TellerSessionStartedEvent.java
+++ src/main/java/com/example/domain/tellersession/model/TellerSessionStartedEvent.java
@@ -5,10 +5,10 @@ import com.example.domain.shared.DomainEvent;
 import java.time.Instant;
 
 public record TellerSessionStartedEvent(
-        String aggregateId,
-        String tellerId,
-        String terminalId,
-        Instant occurredAt
+    String aggregateId,
+    String tellerId,
+    String terminalId,
+    Instant occurredAt
 ) implements DomainEvent {
     @Override
     public String type() {

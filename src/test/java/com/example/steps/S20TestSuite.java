f4c4ef57e6af1f2dc28904519380d4abfc349b7a
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:15:54 -0500

    feat(feature/s-20): src/test/java/com/example/steps/S20TestSuite.java

--- src/test/java/com/example/steps/S20TestSuite.java
+++ src/test/java/com/example/steps/S20TestSuite.java
@@ -4,6 +4,10 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite runner for S-20 Feature.
+ * Run with 'mvn test -Dtest=S20TestSuite' or via IDE JUnit runner.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-20.feature")

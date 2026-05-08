16ef518e1f664b4f8287eb484a6e7411cf5af013
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 09:10:00 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,10 +1,20 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.CucumberJUnitExecutionParameters.GLUE_PROPERTY_NAME;
+
+/**
+ * Test Suite for S-FB-1.
+ * Run this to execute the Red-phase tests.
+ */
 @Suite
-@SelectClasses(SFB1Steps.class)
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class SFB1TestSuite {
-    // Test Suite Runner
+    // This class acts as the entry point for the Cucumber Runner
 }

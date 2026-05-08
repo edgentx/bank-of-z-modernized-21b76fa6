3f3cfda5c8786f262091c032b979e8bed089e0dc
Eric Diana <taylormade7978@hotmail.com>
2026-05-07 22:15:14 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,15 +1,15 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
+/**
+ * JUnit 5 Suite runner to execute Cucumber features for S-18.
+ * Run this class to execute the TellerSession BDD tests.
+ */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@SelectClasspathResource("features/S-18.feature")
 public class S18TestSuite {
 }

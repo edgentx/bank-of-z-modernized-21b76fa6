4d0da17b849cfd279cca37278a602604de06a234
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 07:54:06 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/validation/DefectReportWorkflow.java

--- src/main/java/com/example/domain/validation/DefectReportWorkflow.java
+++ src/main/java/com/example/domain/validation/DefectReportWorkflow.java
@@ -1,26 +1,13 @@
 package com.example.domain.validation;
 
-import com.example.domain.defect.model.DefectAggregate;
 import com.example.domain.defect.model.ReportDefectCmd;
-import com.example.domain.defect.repository.DefectRepository;
+import com.example.domain.defect.model.DefectReportedEvent;
 import com.example.domain.shared.SlackMessageValidator;
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
 
-public class DefectReportWorkflow {
-    private final DefectRepository defectRepository;
-    private final SlackMessageValidator slackMessageValidator;
-
-    public DefectReportWorkflow(DefectRepository defectRepository, SlackMessageValidator slackMessageValidator) {
-        this.defectRepository = defectRepository;
-        this.slackMessageValidator = slackMessageValidator;
-    }
-
-    public void reportDefect(ReportDefectCmd cmd) {
-        DefectAggregate aggregate = new DefectAggregate(cmd.defectId());
-        aggregate.execute(cmd);
-        defectRepository.save(aggregate);
-    }
-
-    public boolean validateMessage(String messageBody) {
-        return slackMessageValidator.isValid(messageBody);
-    }
+@WorkflowInterface
+public interface DefectReportWorkflow {
+    @WorkflowMethod
+    String reportDefect(ReportDefectCmd cmd);
 }

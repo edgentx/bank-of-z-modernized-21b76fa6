bf87f4417eda7480444a07850e8cd9ebedb3d3cc
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:55:05 -0500

    feat(feature/s-20): src/main/java/com/example/domain/teller/model/EndSessionCmd.java

--- src/main/java/com/example/domain/teller/model/EndSessionCmd.java
+++ src/main/java/com/example/domain/teller/model/EndSessionCmd.java
@@ -2,6 +2,4 @@ package com.example.domain.teller.model;
 
 import com.example.domain.shared.Command;
 
-import java.time.Instant;
-
-public record EndSessionCmd(String sessionId, Instant occurredAt) implements Command {}
+public record EndSessionCmd(String sessionId) implements Command {}

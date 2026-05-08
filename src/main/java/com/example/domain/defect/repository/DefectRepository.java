4a46ad7c59a5f833bba02b2367b44566608b3aea
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:01:07 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/defect/repository/DefectRepository.java

--- src/main/java/com/example/domain/defect/repository/DefectRepository.java
+++ src/main/java/com/example/domain/defect/repository/DefectRepository.java
@@ -1,9 +1,10 @@
 package com.example.domain.defect.repository;
 
 import com.example.domain.defect.model.DefectAggregate;
+
 import java.util.Optional;
 
 public interface DefectRepository {
-    DefectAggregate save(DefectAggregate aggregate);
-    Optional<DefectAggregate> findById(String id);
+    void save(DefectAggregate aggregate);
+    Optional<DefectAggregate> findById(String defectId);
 }

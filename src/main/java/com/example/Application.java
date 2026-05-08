d8816a9f1e85f1afa9315029ee70b816ca662f43
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:30:31 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,20 +1,11 @@
 package com.example;
 
-import com.example.adapters.SlackNotificationAdapter;
-import com.example.ports.SlackNotificationPort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.java, args);
-    }
-
-    @Bean
-    public SlackNotificationPort slackNotificationPort() {
-        return new SlackNotificationAdapter();
+        SpringApplication.run(Application.class, args);
     }
 }

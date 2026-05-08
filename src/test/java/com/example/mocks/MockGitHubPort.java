0cb150daa68088c000eeaae7b5d3f350f9e79008
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 03:40:08 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/MockGitHubPort.java

--- src/test/java/com/example/mocks/MockGitHubPort.java
+++ src/test/java/com/example/mocks/MockGitHubPort.java
@@ -1,35 +1,21 @@
 package com.example.mocks;
 
 import com.example.ports.GitHubPort;
-import java.util.Optional;
 
 /**
  * Mock implementation of GitHubPort for testing.
- * Simulates GitHub issue creation and URL generation.
  */
 public class MockGitHubPort implements GitHubPort {
-
-    private boolean shouldSucceed = true;
-    private String mockUrlPrefix = "https://github.com/example/issues/";
-    private int issueCounter = 1;
-
+    
+    private String returnUrl = "https://github.com/mock/issues/1";
+    
     @Override
-    public Optional<String> createIssue(String title, String body) {
-        if (shouldSucceed) {
-            String url = mockUrlPrefix + issueCounter++;
-            System.out.println("[MockGitHub] Created issue " + url + " with title: " + title);
-            return Optional.of(url);
-        } else {
-            System.out.println("[MockGitHub] Failed to create issue.");
-            return Optional.empty();
-        }
+    public String createIssue(String title, String body, String type) {
+        System.out.println("[MockGitHub] Issue Created: " + title);
+        return returnUrl;
     }
 
-    public void setShouldSucceed(boolean shouldSucceed) {
-        this.shouldSucceed = shouldSucceed;
-    }
-
-    public void setMockUrlPrefix(String prefix) {
-        this.mockUrlPrefix = prefix;
+    public void setReturnUrl(String url) {
+        this.returnUrl = url;
     }
 }

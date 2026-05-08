ab33115e3589121660f7d47d4a6d9aab9ceac2f5
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 02:56:49 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,16 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Application entry point.
- * S-FB-1: VForce360 Defect Reporting.
+ * Main entry point for the Bank of Z Modernization application.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

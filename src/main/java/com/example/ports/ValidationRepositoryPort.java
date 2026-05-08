8f5c96e303be3708a826130a9b0853155efec259
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 11:48:37 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/ValidationRepositoryPort.java

--- src/main/java/com/example/ports/ValidationRepositoryPort.java
+++ src/main/java/com/example/ports/ValidationRepositoryPort.java
@@ -1,9 +1,10 @@
 package com.example.ports;
 
 import com.example.domain.validation.model.ValidationAggregate;
+
 import java.util.Optional;
 
 public interface ValidationRepositoryPort {
     void save(ValidationAggregate aggregate);
-    Optional<ValidationAggregate> findById(String validationId);
+    Optional<ValidationAggregate> findById(String id);
 }

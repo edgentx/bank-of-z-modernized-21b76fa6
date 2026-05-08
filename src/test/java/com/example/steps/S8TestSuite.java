8d03549e86b81ee5fb76083b6739eacc61d126cf
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 18:19:13 -0500

    feat(feature/s-8): src/test/java/com/example/steps/S8TestSuite.java

--- src/test/java/com/example/steps/S8TestSuite.java
+++ src/test/java/com/example/steps/S8TestSuite.java
@@ -4,9 +4,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-/**
- * Test Suite Runner for S-8 Cucumber tests.
- */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-8.feature")

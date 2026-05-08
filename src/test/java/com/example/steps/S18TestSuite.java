0f80774eaa49974c0f01cadf17903df52db1601a
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 06:14:01 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,27 +1,15 @@
 package com.example.steps;
 
-import com.example.mocks.InMemoryTellerSessionRepository;
-import com.example.domain.tellersession.repository.TellerSessionRepository;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@CucumberContextConfiguration
-@SpringBootTest(classes = S18TestSuite.TestConfig.class)
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S18TestSuite {
-
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public TellerSessionRepository tellerSessionRepository() {
-            return new InMemoryTellerSessionRepository();
-        }
-    }
 }

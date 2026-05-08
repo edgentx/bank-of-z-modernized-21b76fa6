e8f4f241daa25720af8ccf5fe30066bebdd41cf3
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 01:20:32 -0500

    feat(feature/s-fb-1): src/main/java/com/example/config/RestConfig.java

--- src/main/java/com/example/config/RestConfig.java
+++ src/main/java/com/example/config/RestConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * General REST configuration for the application.
+ */
 @Configuration
 public class RestConfig {
 

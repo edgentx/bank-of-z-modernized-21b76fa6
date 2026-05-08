569345d179d8f7c83fa7497bffc3842b871f3f09
Eric Diana <taylormade7978@hotmail.com>
2026-05-08 00:33:08 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,30 @@
 package com.example;
 
+import com.example.adapters.GitHubAdapter;
+import com.example.adapters.SlackNotificationAdapter;
+import com.example.domain.vforce360.model.VForce360Aggregate;
+import com.example.domain.vforce360.repository.VForce360Repository;
+import com.example.ports.GitHubPort;
+import com.example.ports.SlackNotificationPort;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public GitHubPort gitHubPort() {
+		return new GitHubAdapter();
+	}
+
+	@Bean
+	public SlackNotificationPort slackNotificationPort() {
+		return new SlackNotificationAdapter();
+	}
+
 }

553299951fef5ab3b0ede9de258f404a2c7e8111
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 18:59:27 -0500

    feat(feature/s-19): src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java

--- src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
+++ src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
@@ -1,7 +1,8 @@
 package com.example.mocks;
 
-import com.example.domain.tellersession.model.TellerSessionAggregate;
-import com.example.domain.tellersession.repository.TellerSessionRepository;
+import com.example.domain.tellermenu.model.TellerSessionAggregate;
+import com.example.domain.tellermenu.repository.TellerSessionRepository;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;

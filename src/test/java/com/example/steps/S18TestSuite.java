6388342581c14e796696a7e3199f2ce942143da2
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 18:55:26 -0500

    feat(feature/s-18): src/test/java/com/example/steps/S18TestSuite.java

--- src/test/java/com/example/steps/S18TestSuite.java
+++ src/test/java/com/example/steps/S18TestSuite.java
@@ -1,15 +1,17 @@
 package com.example.steps;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
+/**
+ * JUnit 5 Suite wrapper for Cucumber S-18 tests.
+ * Allows `mvn test` to pick up the feature file and steps.
+ */
 @Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-18.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S18TestSuite {
 }

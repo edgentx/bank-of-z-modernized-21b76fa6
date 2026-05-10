330ce1087dea1f09fcc1d04c5d0c35c43032f4fd
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 18:54:33 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryDefectRepository.java

--- src/test/java/com/example/mocks/InMemoryDefectRepository.java
+++ src/test/java/com/example/mocks/InMemoryDefectRepository.java
@@ -2,13 +2,11 @@ package com.example.mocks;
 
 import com.example.domain.defect.model.DefectAggregate;
 import com.example.domain.defect.port.DefectRepository;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class InMemoryDefectRepository implements DefectRepository {
-
     private final Map<String, DefectAggregate> store = new HashMap<>();
 
     @Override

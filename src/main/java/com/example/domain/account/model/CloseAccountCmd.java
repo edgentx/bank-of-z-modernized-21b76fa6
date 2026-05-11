4a38ed457f03480b406958ec64e8c63a6988c008
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:38:53 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/CloseAccountCmd.java

--- src/main/java/com/example/domain/account/model/CloseAccountCmd.java
+++ src/main/java/com/example/domain/account/model/CloseAccountCmd.java
@@ -2,11 +2,4 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-import java.util.Objects;
-
-public record CloseAccountCmd(String accountId, String accountNumber) implements Command {
-    public CloseAccountCmd {
-        Objects.requireNonNull(accountId);
-        Objects.requireNonNull(accountNumber);
-    }
-}
+public record CloseAccountCmd(String accountId, String accountNumber) implements Command {}

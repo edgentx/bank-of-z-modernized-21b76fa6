ddd4a472e3d5c8b2541d1339dc8fab46fc962701
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 08:31:27 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -4,11 +4,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+/**
+ * Test Suite for S-FB-1.
+ * Run this class to execute the regression test for the GitHub URL validation.
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "features/S-FB-1.feature",
+    features = {"features/S-FB-1.feature"},
     glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber/S-FB-1.html"}
+    plugin = {"pretty", "html:target/cucumber/S-FB-1.html"},
+    monochrome = true
 )
 public class SFB1TestSuite {
+    // This class serves as the entry point for the JUnit runner to execute Cucumber features.
 }

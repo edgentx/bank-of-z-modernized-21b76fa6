d3aa83c35437272b861898127ef56503ba48f651
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:50:08 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,19 +1,17 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 /**
- * Test Suite configuration for S-FB-1.
- * Maps to the feature file.
+ * Test Suite for S-FB-1.
+ * Maps to features/S-FB-1.feature
  */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-FB-1.feature")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@Cucumber
 public class SFB1TestSuite {
 }

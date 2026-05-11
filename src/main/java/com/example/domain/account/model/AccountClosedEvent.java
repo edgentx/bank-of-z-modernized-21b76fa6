a1c52dc3c78c2f33708407cebf805df9de8283ab
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:24:38 -0500

    feat(feature/s-7): src/main/java/com/example/domain/account/model/AccountClosedEvent.java

--- src/main/java/com/example/domain/account/model/AccountClosedEvent.java
+++ src/main/java/com/example/domain/account/model/AccountClosedEvent.java
@@ -3,6 +3,7 @@ package com.example.domain.account.model;
 import com.example.domain.shared.DomainEvent;
 
 import java.time.Instant;
+import java.util.UUID;
 
 public record AccountClosedEvent(String accountNumber, Instant occurredAt) implements DomainEvent {
     @Override
@@ -13,10 +14,5 @@ public record AccountClosedEvent(String accountNumber, Instant occurredAt) imple
     @Override
     public String aggregateId() {
         return accountNumber;
-    }
-
-    @Override
-    public Instant occurredAt() {
-        return occurredAt;
     }
 }

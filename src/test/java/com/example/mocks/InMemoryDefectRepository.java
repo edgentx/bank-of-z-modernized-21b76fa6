070f4351764bf0fea0734cb0ae842281c39041ef
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:13:58 -0500

    feat(feature/s-fb-1): src/test/java/com/example/mocks/InMemoryDefectRepository.java

--- src/test/java/com/example/mocks/InMemoryDefectRepository.java
+++ src/test/java/com/example/mocks/InMemoryDefectRepository.java
@@ -2,18 +2,20 @@ package com.example.mocks;
 
 import com.example.domain.defect.model.DefectAggregate;
 import com.example.domain.defect.repository.DefectRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Component
 public class InMemoryDefectRepository implements DefectRepository {
+
     private final Map<String, DefectAggregate> store = new HashMap<>();
 
     @Override
-    public DefectAggregate save(DefectAggregate aggregate) {
+    public void save(DefectAggregate aggregate) {
         store.put(aggregate.id(), aggregate);
-        return aggregate;
     }
 
     @Override

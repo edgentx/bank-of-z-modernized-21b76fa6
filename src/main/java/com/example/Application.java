a88b37a4e0e365e091778196ce932381b2473493
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:59:39 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,39 +1,18 @@
 package com.example;
 
-import com.example.adapters.GitHubAdapter;
-import com.example.adapters.SlackNotificationAdapter;
-import com.example.ports.GitHubPort;
-import com.example.ports.SlackNotificationPort;
-import com.example.service.ReportDefectService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.example.domain",
+    "com.example.ports",
+    "com.example.adapters",
+    "com.example.mocks"
+})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public SlackNotificationPort slackNotificationPort(SlackNotificationAdapter adapter) {
-        return adapter;
-    }
-
-    @Bean
-    public GitHubPort gitHubPort(GitHubAdapter adapter) {
-        return adapter;
-    }
-
-    @Bean
-    public ReportDefectService reportDefectService(GitHubPort gitHubPort, SlackNotificationPort slackNotificationPort) {
-        return new ReportDefectService(gitHubPort, slackNotificationPort);
     }
 }

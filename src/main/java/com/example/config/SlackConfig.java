b5bd840749682dc7470fc1ae43b64b569c5bd8b1
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:14:07 -0500

    feat(feature/s-fb-1): src/main/java/com/example/config/SlackConfig.java

--- src/main/java/com/example/config/SlackConfig.java
+++ src/main/java/com/example/config/SlackConfig.java
@@ -1,14 +1,23 @@
 package com.example.config;
 
-import com.example.mocks.InMemorySlackNotificationPort;
-import com.example.services.SlackNotificationPort;
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "app.slack.enabled", havingValue = "true", matchIfMissing = false)
 public class SlackConfig {
+
     @Bean
-    public SlackNotificationPort slackNotificationPort() {
-        return new InMemorySlackNotificationPort();
+    public Slack slack() {
+        return Slack.getInstance();
+    }
+
+    @Bean
+    public MethodsClient methodsClient(Slack slack, @Value("${app.slack.token}") String token) {
+        return slack.methods(token);
     }
 }

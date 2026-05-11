e4d6a4dbfb805e2772793853440dbacf64fdd121
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 22:38:22 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java

--- src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
+++ src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
@@ -2,4 +2,14 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-public record UpdateAccountStatusCmd(String accountNumber, String newStatus) implements Command {}
+import java.util.Objects;
+
+public record UpdateAccountStatusCmd(
+        String targetAccountNumber,
+        AccountAggregate.AccountStatus newStatus
+) implements Command {
+    public UpdateAccountStatusCmd {
+        Objects.requireNonNull(targetAccountNumber);
+        Objects.requireNonNull(newStatus);
+    }
+}

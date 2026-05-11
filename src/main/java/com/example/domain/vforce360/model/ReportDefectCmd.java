f7ea4323d950a0d2ed640680288b4f1bdf836b31
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:51:51 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java

--- src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java
+++ src/main/java/com/example/domain/vforce360/model/ReportDefectCmd.java
@@ -2,10 +2,4 @@ package com.example.domain.vforce360.model;
 
 import com.example.domain.shared.Command;
 
-public record ReportDefectCmd(
-    String defectId,
-    String project,
-    String severity,
-    String description
-) implements Command {
-}
+public record ReportDefectCmd(String conversationId, String defectId) implements Command {}

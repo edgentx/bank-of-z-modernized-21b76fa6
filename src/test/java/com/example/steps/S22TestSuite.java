cdc5c00c6f9acbfe0b9b60569f2da83e02438542
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 00:34:55 -0500

    feat(feature/s-22): src/test/java/com/example/steps/S22TestSuite.java

--- src/test/java/com/example/steps/S22TestSuite.java
+++ src/test/java/com/example/steps/S22TestSuite.java
@@ -1,3 +1,5 @@
+package com.example.steps;
+
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;

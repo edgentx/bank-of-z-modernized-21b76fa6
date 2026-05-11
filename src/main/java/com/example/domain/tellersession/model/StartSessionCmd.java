3c81b5c30a024a7d6735427a25c6b45cdc9b7cbc
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 08:36:56 -0500

    feat(feature/s-18): src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java

--- src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java
+++ src/main/java/com/example/domain/tellersession/model/StartSessionCmd.java
@@ -2,4 +2,4 @@ package com.example.domain.tellersession.model;
 
 import com.example.domain.shared.Command;
 
-public record StartSessionCmd(String tellerId, String terminalId) implements Command {}
+public record StartSessionCmd(String aggregateId, String tellerId, String terminalId) implements Command {}

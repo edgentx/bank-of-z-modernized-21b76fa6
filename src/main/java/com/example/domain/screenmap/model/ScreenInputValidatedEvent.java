9b692fee9abbb908c1bce4524183a2f197b39d66
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 21:35:51 -0500

    feat(feature/s-22): src/main/java/com/example/domain/screenmap/model/ScreenInputValidatedEvent.java

--- src/main/java/com/example/domain/screenmap/model/ScreenInputValidatedEvent.java
+++ src/main/java/com/example/domain/screenmap/model/ScreenInputValidatedEvent.java
@@ -5,13 +5,7 @@ import com.example.domain.shared.DomainEvent;
 import java.time.Instant;
 import java.util.Map;
 
-public record ScreenInputValidatedEvent(
-        String aggregateId,
-        String screenId,
-        Map<String, String> inputs,
-        Instant occurredAt
-) implements DomainEvent {
-
+public record ScreenInputValidatedEvent(String aggregateId, Map<String, String> inputFields, Instant occurredAt) implements DomainEvent {
     @Override
     public String type() {
         return "input.validated";

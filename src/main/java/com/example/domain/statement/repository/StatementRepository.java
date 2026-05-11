1f944f0a77c624598366f50bde555635f3432c21
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:48:50 -0500

    feat(feature/s-9): src/main/java/com/example/domain/statement/repository/StatementRepository.java

--- src/main/java/com/example/domain/statement/repository/StatementRepository.java
+++ src/main/java/com/example/domain/statement/repository/StatementRepository.java
@@ -1,9 +1,12 @@
 package com.example.domain.statement.repository;
 
 import com.example.domain.statement.model.StatementAggregate;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public interface StatementRepository {
-    StatementAggregate save(StatementAggregate aggregate);
-    Optional<StatementAggregate> findById(String statementId);
+    Optional<StatementAggregate> findById(String id);
+    void save(StatementAggregate aggregate);
 }

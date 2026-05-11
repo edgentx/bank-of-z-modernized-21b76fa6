811c1c0b57c01f99cdc9d8a6ddbb59b6b322bb5b
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:57:52 -0500

    feat(feature/s-fb-1): src/main/java/com/example/domain/defect/model/LinkGitHubIssueCmd.java

--- src/main/java/com/example/domain/defect/model/LinkGitHubIssueCmd.java
+++ src/main/java/com/example/domain/defect/model/LinkGitHubIssueCmd.java
@@ -2,7 +2,4 @@ package com.example.domain.defect.model;
 
 import com.example.domain.shared.Command;
 
-public record LinkGitHubIssueCmd(
-    String defectId,
-    String url
-) implements Command {}
+public record LinkGitHubIssueCmd(String defectId, String url) implements Command {}

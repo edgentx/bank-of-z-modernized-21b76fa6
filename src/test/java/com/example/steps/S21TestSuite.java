c6a9b0e871e791a05d6891741f5c98c2e9535aea
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 21:16:17 -0500

    feat(feature/s-21): src/test/java/com/example/steps/S21TestSuite.java

--- src/test/java/com/example/steps/S21TestSuite.java
+++ src/test/java/com/example/steps/S21TestSuite.java
@@ -4,8 +4,11 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * JUnit 5 Suite runner for Cucumber S-21 tests.
+ */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-21.feature")
 public class S21TestSuite {
 }

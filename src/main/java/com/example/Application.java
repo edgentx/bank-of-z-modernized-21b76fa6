5eca5ecf127ed1bcb1b1fecfaf57514dcef31073
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 19:10:55 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,33 +1,13 @@
 package com.example;
 
-import com.example.adapters.SlackNotificationAdapter;
-import com.example.adapters.GitHubIssueAdapter;
-import com.example.ports.GitHubIssuePort;
-import com.example.ports.SlackNotificationPort;
-import com.example.workflow.ReportDefectWorkflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.domain", "com.example.adapters", "com.example.config", "com.example.ports"})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public GitHubIssuePort gitHubIssuePort() {
-        return new GitHubIssueAdapter();
-    }
-
-    @Bean
-    public SlackNotificationPort slackNotificationPort() {
-        return new SlackNotificationAdapter();
-    }
-
-    @Bean
-    public ReportDefectWorkflow reportDefectWorkflow(GitHubIssuePort gitHubIssuePort, SlackNotificationPort slackNotificationPort) {
-        return new ReportDefectWorkflow(gitHubIssuePort, slackNotificationPort);
     }
 }

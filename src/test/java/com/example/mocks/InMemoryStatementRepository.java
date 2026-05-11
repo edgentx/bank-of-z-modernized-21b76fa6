49f95c6b0473625bd94d1d9bfa7f24a6387acc11
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:44:15 -0500

    feat(feature/s-8): src/test/java/com/example/mocks/InMemoryStatementRepository.java

--- src/test/java/com/example/mocks/InMemoryStatementRepository.java
+++ src/test/java/com/example/mocks/InMemoryStatementRepository.java
@@ -2,7 +2,6 @@ package com.example.mocks;
 
 import com.example.domain.statement.model.StatementAggregate;
 import com.example.domain.statement.repository.StatementRepository;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -11,12 +10,13 @@ public class InMemoryStatementRepository implements StatementRepository {
     private final Map<String, StatementAggregate> store = new HashMap<>();
 
     @Override
-    public Optional<StatementAggregate> findById(String id) {
-        return Optional.ofNullable(store.get(id));
+    public StatementAggregate save(StatementAggregate aggregate) {
+        store.put(aggregate.id(), aggregate);
+        return aggregate;
     }
 
     @Override
-    public void save(StatementAggregate aggregate) {
-        store.put(aggregate.id(), aggregate);
+    public Optional<StatementAggregate> findById(String id) {
+        return Optional.ofNullable(store.get(id));
     }
 }

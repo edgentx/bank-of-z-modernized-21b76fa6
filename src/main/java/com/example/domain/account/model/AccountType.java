ce3513f1a371b039b1537ea8f70ef1481e575472
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:05:53 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/AccountType.java

--- src/main/java/com/example/domain/account/model/AccountType.java
+++ src/main/java/com/example/domain/account/model/AccountType.java
@@ -1,19 +1,6 @@
 package com.example.domain.account.model;
 
-import java.math.BigDecimal;
-
 public enum AccountType {
-    SAVINGS(BigDecimal.valueOf(100.00)),
-    CHECKING(BigDecimal.valueOf(0.00)),
-    INVESTMENT(BigDecimal.valueOf(1000.00));
-
-    private final BigDecimal minBalance;
-
-    AccountType(BigDecimal minBalance) {
-        this.minBalance = minBalance;
-    }
-
-    public BigDecimal getMinBalance() {
-        return minBalance;
-    }
+    SAVINGS,
+    CHECKING
 }

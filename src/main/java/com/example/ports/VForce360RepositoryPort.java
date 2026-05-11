9cc8549ff804a0a17d3680172202f099b9fbe3b5
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:07:14 -0500

    feat(feature/s-fb-1): src/main/java/com/example/ports/VForce360RepositoryPort.java

--- src/main/java/com/example/ports/VForce360RepositoryPort.java
+++ src/main/java/com/example/ports/VForce360RepositoryPort.java
@@ -1,13 +1,10 @@
 package com.example.ports;
 
-import com.example.domain.vforce360.model.VForce360Aggregate;
-
+import com.example.domain.defect.model.DefectAggregate;
+import com.example.domain.defect.model.ReportDefectCmd;
 import java.util.Optional;
 
-/**
- * Port interface for VForce360 Repository.
- */
 public interface VForce360RepositoryPort {
-    VForce360Aggregate save(VForce360Aggregate aggregate);
-    Optional<VForce360Aggregate> findById(String id);
+    DefectAggregate save(DefectAggregate aggregate);
+    Optional<DefectAggregate> findById(String id);
 }

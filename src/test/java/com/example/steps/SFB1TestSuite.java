5e094895283f444922e45f2c29bd443fbeecfe80
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:14:00 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -6,11 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {"classpath:features/S-FB-1.feature"},
+    features = "../../features/S-FB-1.feature",
     glue = {"com.example.steps"},
-    plugin = {"pretty", "html:target/cucumber-report/S-FB-1"},
-    monochrome = true
+    plugin = {"pretty", "html:target/cucumber-report/S-FB-1.html"}
 )
 public class SFB1TestSuite {
-    // Test runner configuration for S-FB-1
 }

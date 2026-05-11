dd9eee39e41e5425f6b4336d8297e0f32b6bc6df
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 02:53:33 -0500

    feat(feature/s-8): src/test/java/com/example/steps/S8TestSuite.java

--- src/test/java/com/example/steps/S8TestSuite.java
+++ src/test/java/com/example/steps/S8TestSuite.java
@@ -1,11 +1,11 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@IncludeTags("S-8") // Assuming we might tag features, or simply relying on resource discovery
 public class S8TestSuite {
 }

81500dfe566d24d48483ca06bcca6d11153a268a
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 01:27:18 -0500

    feat(feature/s-5): src/test/java/com/example/steps/S5TestSuite.java

--- src/test/java/com/example/steps/S5TestSuite.java
+++ src/test/java/com/example/steps/S5TestSuite.java
@@ -6,6 +6,6 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/S-5.feature")
 public class S5TestSuite {
 }

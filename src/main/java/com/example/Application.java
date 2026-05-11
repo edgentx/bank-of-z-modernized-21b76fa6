d13ab2037fdef944c23486625f181da80c50c585
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:56:09 -0500

    feat(feature/s-fb-1): src/main/java/com/example/Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Main application entry point for VForce360 Validation Fix.
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
 public class Application {

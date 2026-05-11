0fd7017e095eef7103978d65087c706cb3e79364
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 21:11:31 -0500

    feat(feature/s-21): src/main/java/com/example/domain/userinterface/model/RenderScreenCmd.java

--- src/main/java/com/example/domain/userinterface/model/RenderScreenCmd.java
+++ src/main/java/com/example/domain/userinterface/model/RenderScreenCmd.java
@@ -2,12 +2,8 @@ package com.example.domain.userinterface.model;
 
 import com.example.domain.shared.Command;
 
-/**
- * Command to request the rendering of a specific screen layout.
- */
 public record RenderScreenCmd(
-        String screenMapId,
-        String screenId,
-        String deviceType
-) implements Command {
-}
+    String screenMapId,
+    String screenId,
+    String deviceType
+) implements Command {}

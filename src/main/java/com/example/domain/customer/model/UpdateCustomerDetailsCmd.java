d0b031443d8f61dacf8a582da81dfad7b3a5771c
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 10:41:17 -0500

    feat(feature/s-3): src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java

--- src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
+++ src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
@@ -2,10 +2,14 @@ package com.example.domain.customer.model;
 
 import com.example.domain.shared.Command;
 
+import java.time.Instant;
+
 public record UpdateCustomerDetailsCmd(
-    String customerId,
-    String emailAddress,
-    String sortCode,
-    String fullName,
-    String dateOfBirth
+        String customerId,
+        String newFullName,
+        String newEmailAddress,
+        String newSortCode,
+        String governmentId,
+        Instant dateOfBirth,
+        boolean hasActiveAccounts // Used to enforce the 'active accounts' invariant
 ) implements Command {}

ee6329ee5b118bb998f41e023a4100f23228bd0c
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:50:38 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/VW454TestSuite.java

--- src/test/java/com/example/steps/VW454TestSuite.java
+++ src/test/java/com/example/steps/VW454TestSuite.java
@@ -1,11 +1,19 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features/VW454.feature")
+/**
+ * Test Suite for VW-454 Regression.
+ * Connects the Step Definitions to the Feature File.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "features/VW-454.feature",
+    glue = {"com.example.steps"},
+    plugin = {"pretty", "html:target/cucumber-report/VW-454.html"},
+    monochrome = true
+)
 public class VW454TestSuite {
 }

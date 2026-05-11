f363b1a53e0cbdf7d48404113383b007cacd6300
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 01:33:18 -0500

    feat(feature/s-3): src/main/java/com/example/domain/customer/model/CustomerDetailsUpdatedEvent.java

--- src/main/java/com/example/domain/customer/model/CustomerDetailsUpdatedEvent.java
+++ src/main/java/com/example/domain/customer/model/CustomerDetailsUpdatedEvent.java
@@ -6,10 +6,12 @@ import java.time.Instant;
 
 public record CustomerDetailsUpdatedEvent(
         String customerId,
-        String emailAddress,
+        String fullName,
+        String email,
         String sortCode,
         Instant occurredAt
 ) implements DomainEvent {
+
     @Override
     public String type() {
         return "customer.details.updated";

a90a2a2050326c9f3740c6d4885850add0a6feb7
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 00:24:58 -0500

    feat(feature/s-22): src/main/java/com/example/domain/userinterfacenavigation/model/ValidateScreenInputCmd.java

--- src/main/java/com/example/domain/userinterfacenavigation/model/ValidateScreenInputCmd.java
+++ src/main/java/com/example/domain/userinterfacenavigation/model/ValidateScreenInputCmd.java
@@ -3,5 +3,4 @@ package com.example.domain.userinterfacenavigation.model;
 import com.example.domain.shared.Command;
 import java.util.Map;
 
-public record ValidateScreenInputCmd(String screenId, Map<String, String> inputFields) implements Command {
-}
+public record ValidateScreenInputCmd(String screenId, Map<String, String> inputFields) implements Command {}

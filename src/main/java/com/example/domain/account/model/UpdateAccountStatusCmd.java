6dc4d153cad6901543a8a236b4a25f286dffe603
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 22:14:16 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java

--- src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
+++ src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
@@ -1,5 +1,12 @@
 package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
+import java.math.BigDecimal;
 
-public record UpdateAccountStatusCmd(String accountNumber, String newStatus) implements Command {}
+public record UpdateAccountStatusCmd(
+  String accountId,
+  String accountNumber,
+  String newStatus,
+  BigDecimal currentBalance,
+  String accountType
+) implements Command {}

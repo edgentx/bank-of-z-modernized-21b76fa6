ad2b25f773519418ef82d216e1f2dfc0b28a1e36
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:57:56 -0500

    feat(feature/s-fb-1): src/test/java/com/example/steps/SFB1TestSuite.java

--- src/test/java/com/example/steps/SFB1TestSuite.java
+++ src/test/java/com/example/steps/SFB1TestSuite.java
@@ -1,15 +1,15 @@
 package com.example.steps;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "features/S-FB-1.feature",
-    glue = {"com.example.steps"},
-    plugin = {"pretty", "summary"}
-)
+@Suite
+@IncludeEngines(Cucumber.class)
+@SelectClasspathResource("features")
+// Additional configuration to ensure strict run
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "@S-FB-1")
 public class SFB1TestSuite {
-    // Cucumber Test Suite for S-FB-1
 }

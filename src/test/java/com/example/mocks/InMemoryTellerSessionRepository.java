964ef10f73b84f8d1d08902f48554537042fdc5d
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:34:27 -0500

    feat(feature/s-18): src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java

--- src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
+++ src/test/java/com/example/mocks/InMemoryTellerSessionRepository.java
@@ -1,7 +1,8 @@
 package com.example.mocks;
 
-import com.example.domain.tellermaintenance.repository.TellerSessionRepository;
-import com.example.domain.teller.model.TellerSessionAggregate;
+import com.example.domain.tellersession.model.TellerSessionAggregate;
+import com.example.domain.tellersession.repository.TellerSessionRepository;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -18,5 +19,10 @@ public class InMemoryTellerSessionRepository implements TellerSessionRepository 
     @Override
     public Optional<TellerSessionAggregate> findById(String id) {
         return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public TellerSessionAggregate load(String id) {
+        return findById(id).orElseThrow(() -> new IllegalArgumentException("Aggregate not found: " + id));
     }
 }

b60e0b325d4f59d8f208df2a3e2a3df3aa7716f9
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 09:55:53 -0500

    feat(feature/s-22): src/test/java/com/example/steps/S22TestSuite.java

--- src/test/java/com/example/steps/S22TestSuite.java
+++ src/test/java/com/example/steps/S22TestSuite.java
@@ -12,5 +12,4 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
 public class S22TestSuite {
-    // This empty class acts as a runner for the Cucumber features
 }

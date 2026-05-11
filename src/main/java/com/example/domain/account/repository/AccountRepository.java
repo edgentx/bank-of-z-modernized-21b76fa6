6d22ced7a96c117579b88acd3ff3cdf70dcbc485
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 22:13:23 -0500

    feat(feature/s-5): src/main/java/com/example/domain/account/repository/AccountRepository.java

--- src/main/java/com/example/domain/account/repository/AccountRepository.java
+++ src/main/java/com/example/domain/account/repository/AccountRepository.java
@@ -1,10 +1,9 @@
 package com.example.domain.account.repository;
 
 import com.example.domain.account.model.AccountAggregate;
-
 import java.util.Optional;
 
 public interface AccountRepository {
-    AccountAggregate save(AccountAggregate aggregate);
+    void save(AccountAggregate aggregate);
     Optional<AccountAggregate> findById(String id);
 }

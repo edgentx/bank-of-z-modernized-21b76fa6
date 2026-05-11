4292b8b1563860a49617bfef6a63b2cd0471b2a0
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:57:26 -0500

    feat(feature/s-19): src/test/java/com/example/steps/S19TestSuite.java

--- src/test/java/com/example/steps/S19TestSuite.java
+++ src/test/java/com/example/steps/S19TestSuite.java
@@ -1,13 +1,11 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = "glue", value = "com.example.steps")
+@SelectClasspathResource("features/S-19.feature")
 public class S19TestSuite {
 }

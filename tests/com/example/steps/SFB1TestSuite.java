a853c5377221ce33f7df651264b2d805ed21fd95
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 20:08:04 -0500

    feat(feature/s-fb-1): tests/com/example/steps/SFB1TestSuite.java

---
+++ tests/com/example/steps/SFB1TestSuite.java
@@ -0,0 +1,12 @@
+package com.example.steps;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasses({SFB1Steps.class})
+public class SFB1TestSuite {
+    // Cucumber test suite configuration
+}

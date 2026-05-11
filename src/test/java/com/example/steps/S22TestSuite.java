2cd8cb78d52461c89226edab95c83978b368d106
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 00:35:29 -0500

    feat(feature/s-22): src/test/java/com/example/steps/S22TestSuite.java

--- src/test/java/com/example/steps/S22TestSuite.java
+++ src/test/java/com/example/steps/S22TestSuite.java
@@ -4,6 +4,9 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+/**
+ * Test Suite for running S-22 Cucumber features.
+ */
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features/S-22.feature")

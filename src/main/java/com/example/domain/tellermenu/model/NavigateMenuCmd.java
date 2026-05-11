fc1d691bc19537dd3019ab7467592a10d9d93b77
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 23:52:39 -0500

    feat(feature/s-19): src/main/java/com/example/domain/tellermenu/model/NavigateMenuCmd.java

--- src/main/java/com/example/domain/tellermenu/model/NavigateMenuCmd.java
+++ src/main/java/com/example/domain/tellermenu/model/NavigateMenuCmd.java
@@ -2,4 +2,5 @@ package com.example.domain.tellermenu.model;
 
 import com.example.domain.shared.Command;
 
-public record NavigateMenuCmd(String sessionId, String menuId, String action) implements Command {}
+public record NavigateMenuCmd(String menuId, String action) implements Command {
+}

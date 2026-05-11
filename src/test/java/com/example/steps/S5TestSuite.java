72349ef255e67fe06c3e0b6f8d128188b06b4336
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 21:59:36 -0500

    feat(feature/s-5): src/test/java/com/example/steps/S5TestSuite.java

--- src/test/java/com/example/steps/S5TestSuite.java
+++ src/test/java/com/example/steps/S5TestSuite.java
@@ -1,14 +1,11 @@
 package com.example.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.CucumberJUnitPlatformRunnerOptions.GLUE_PROPERTY_NAME;
-
 @Suite
-@SelectClasspathResource("features")
-@SelectClasspathResource("features/S-5.feature") // Explicitly run S-5
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.steps")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/S-5.feature")
 public class S5TestSuite {
 }

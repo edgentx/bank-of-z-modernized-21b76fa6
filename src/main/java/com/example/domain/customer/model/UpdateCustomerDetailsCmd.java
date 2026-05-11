b3863a1fefd34151310b8c535ed8867ba9a16d72
Eric Diana <taylormade7978@hotmail.com>
2026-05-11 01:00:54 -0500

    feat(feature/s-3): src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java

--- src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
+++ src/main/java/com/example/domain/customer/model/UpdateCustomerDetailsCmd.java
@@ -2,12 +2,4 @@ package com.example.domain.customer.model;
 
 import com.example.domain.shared.Command;
 
-import java.time.LocalDate;
-
-public record UpdateCustomerDetailsCmd(
-        String customerId,
-        String fullName,
-        String email,
-        LocalDate dateOfBirth,
-        String sortCode
-) implements Command {}
+public record UpdateCustomerDetailsCmd(String customerId, String fullName, String email, String sortCode) implements Command {}

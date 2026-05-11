adc475ba0534cd8c4154431f497282b68fdd0286
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 22:29:14 -0500

    feat(feature/s-6): src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java

--- src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
+++ src/main/java/com/example/domain/account/model/UpdateAccountStatusCmd.java
@@ -2,13 +2,8 @@ package com.example.domain.account.model;
 
 import com.example.domain.shared.Command;
 
-import java.math.BigDecimal;
-
 /**
  * Command to update the status of an Account.
  */
-public record UpdateAccountStatusCmd(
-    String accountNumber,
-    AccountAggregate.AccountStatus newStatus,
-    BigDecimal contextBalance // Used to pass balance context for invariant checking in test
-) implements Command {}
+public record UpdateAccountStatusCmd(String accountNumber, String newStatus) implements Command {
+}

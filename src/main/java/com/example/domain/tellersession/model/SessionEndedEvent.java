fcef5f6eb20fda71723304a0f52e96b6aa8c887f
Eric Diana <taylormade7978@hotmail.com>
2026-05-10 19:10:13 -0500

    feat(feature/s-20): src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java

--- src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java
+++ src/main/java/com/example/domain/tellersession/model/SessionEndedEvent.java
@@ -1,8 +1,8 @@
 package com.example.domain.tellersession.model;
 
 import com.example.domain.shared.DomainEvent;
+
 import java.time.Instant;
-import java.util.UUID;
 
 public class SessionEndedEvent implements DomainEvent {
     private final String aggregateId;
